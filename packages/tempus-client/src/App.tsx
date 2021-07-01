af60cd14500bbf7e191447a0e9bf5d9a63089489
Daniele Damiani <danieledamianiuk@gmail.com>
2021-07-01 18:15:04 +0100

    Convert to monorepo

---
+++ packages/tempus-client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Using Lerna!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
