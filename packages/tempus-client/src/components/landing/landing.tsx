c17b8c20b1ecffcce5d00f3a4d15532e1505e1ba
Lazar Mitic <Lazar__93@hotmail.com>
2021-07-06 07:45:50 +0200

    move chart grid style to landing section

--- packages/tempus-client/src/components/landing/landing.tsx
+++ packages/tempus-client/src/components/landing/landing.tsx
@@ -4,14 +4,14 @@ import './landing.scss';
 
 function Landing(): JSX.Element {
   return (
-    <>
+    <div className='landing-section-container'>
       <div className='tvl-chart-container'>
         <Chart type={ChartType.TLV} title='Total Value Locked'/>
       </div>
       <div className='volume-chart-container'>
         <Chart type={ChartType.VOLUME} title='Volume 24H' />
       </div>
-    </>
+    </div>
   );
 }
 export default Landing;
