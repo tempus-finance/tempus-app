2fa9ddad92301b870bac7f21aa32fa98298aa7b4
Daniele Damiani <danieledamianiuk@gmail.com>
2021-07-07 10:53:04 +0100

    Improve testing framework - cleanup dependencies (#9)
    * Improve testing framework - cleanup dependencies

--- packages/tempus-client/src/components/wallet-connect/wallet-connect.tsx
+++ packages/tempus-client/src/components/wallet-connect/wallet-connect.tsx
@@ -1,39 +1,39 @@
+import { FC } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
-const injectedConnector = new InjectedConnector({
-  supportedChainIds: [
-    // 1, // Mainnet - commented out just to avoid connecting to it by accident - enable when we go on mainnet
-    3, // Ropsten
-    4, // Rinkeby
-    5, // Goerli 31337
-    42, // Kovan
-    1337 // Local
-  ],
-})
-
-function WalletConnect(): JSX.Element {
+const WalletConnect: FC = (): JSX.Element => {
   const { account, activate, active } = useWeb3React<Web3Provider>();
 
   const onConnect = () => {
-    activate(injectedConnector);
+    if (!active) {
+      const injectedConnector = new InjectedConnector({ supportedChainIds });
+      activate(injectedConnector);
+    }
+  };
+
+  let shortenedAccount;
+  if (account) {
+    shortenedAccount = `${account.substring(0, 6)}...${account.substring(account.length - 5, account.length)}`;
   }
 
   return (
-    <>
-      {/* In case user did not connect wallet yer, show connect wallet option */}
-      {!active &&
-      <p className='header-action' onClick={onConnect}>
-        CONNECT WALLET
-      </p>}
-
-      {/* If use connected a wallet, show wallet address */}
-      {active &&
-      <p className='header-action'>
-        {account?.substring(0, 6)}...{account?.substring(account.length - 5, account.length)}
-      </p>}
-    </>
+    <p className="header-action" onClick={onConnect}>
+      {active ? shortenedAccount : CONNECT_WALLET}
+    </p>
   );
-}
+};
+
 export default WalletConnect;
+
+export const CONNECT_WALLET = 'CONNECT WALLET';
+
+const supportedChainIds = [
+  // 1, // Mainnet - commented out just to avoid connecting to it by accident - enable when we go on mainnet
+  3, // Ropsten
+  4, // Rinkeby
+  5, // Goerli 31337
+  42, // Kovan
+  1337, // Local
+];
