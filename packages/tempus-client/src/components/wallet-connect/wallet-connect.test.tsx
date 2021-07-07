4268bfaee84cbd38cd96c6069a3020f10de8535d
Daniele Damiani <danieledamianiuk@gmail.com>
2021-07-07 11:07:28 +0300

    Improve testing framework - cleanup dependencies

---
+++ packages/tempus-client/src/components/wallet-connect/wallet-connect.test.tsx
@@ -0,0 +1,51 @@
+import { fireEvent, render } from '@testing-library/react';
+import WalletConnect, { CONNECT_WALLET } from './wallet-connect';
+
+jest.mock('@web3-react/core');
+const { useWeb3React } = require('@web3-react/core');
+
+describe('WalletConnect', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('shows default message when wallet not connected', () => {
+    useWeb3React.mockImplementation(() => ({
+      account: undefined,
+      activate: jest.fn,
+      active: false,
+    }));
+
+    const { getByText } = render(<WalletConnect />);
+
+    expect(getByText(CONNECT_WALLET)).toBeInTheDocument();
+  });
+
+  test(`calls "activate" method when click on "${CONNECT_WALLET}"`, () => {
+    const mockActivate = jest.fn();
+
+    useWeb3React.mockImplementation(() => ({
+      account: undefined,
+      activate: mockActivate,
+      active: false,
+    }));
+
+    const { getByText } = render(<WalletConnect />);
+
+    fireEvent.click(getByText(CONNECT_WALLET));
+
+    expect(mockActivate).toHaveBeenCalledTimes(1);
+  });
+
+  test('shows user address when wallet is connected', () => {
+    useWeb3React.mockImplementation(() => ({
+      account: 'ABC-123-XYZ',
+      activate: jest.fn,
+      active: true,
+    }));
+
+    const { getByText } = render(<WalletConnect />);
+
+    expect(getByText(/ABC-12...3-XYZ/i)).toBeInTheDocument();
+  });
+});
--- packages/tempus-client/src/components/wallet-connect/wallet-connect.tsx
+++ packages/tempus-client/src/components/wallet-connect/wallet-connect.tsx
@@ -1,39 +1,38 @@
+import { FC } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import { InjectedConnector } from '@web3-react/injected-connector';
 
-const injectedConnector = new InjectedConnector({
-  supportedChainIds: [
-    // 1, // Mainnet - commented out just to avoid connecting to it by accident - enable when we go on mainnet
-    3, // Ropsten
-    4, // Rinkeby
-    5, // Goerli 31337
-    42, // Kovan
-    1337 // Local
-  ],
-})
+const WalletConnect: FC = (): JSX.Element => {
+  const injectedConnector = new InjectedConnector({ supportedChainIds });
 
-function WalletConnect(): JSX.Element {
   const { account, activate, active } = useWeb3React<Web3Provider>();
+
+  let shortenedAccount;
+  if (account) {
+    shortenedAccount = `${account.substring(0, 6)}...${account.substring(account.length - 5, account.length)}`;
+  }
 
   const onConnect = () => {
     activate(injectedConnector);
-  }
+  };
 
   return (
-    <>
-      {/* In case user did not connect wallet yer, show connect wallet option */}
-      {!active &&
-      <p className='header-action' onClick={onConnect}>
-        CONNECT WALLET
-      </p>}
-
-      {/* If use connected a wallet, show wallet address */}
-      {active &&
-      <p className='header-action'>
-        {account?.substring(0, 6)}...{account?.substring(account.length - 5, account.length)}
-      </p>}
-    </>
+    <p className="header-action" onClick={onConnect}>
+      {active ? shortenedAccount : CONNECT_WALLET}
+    </p>
   );
-}
+};
+
 export default WalletConnect;
+
+export const CONNECT_WALLET = 'CONNECT WALLET';
+
+const supportedChainIds = [
+  // 1, // Mainnet - commented out just to avoid connecting to it by accident - enable when we go on mainnet
+  3, // Ropsten
+  4, // Rinkeby
+  5, // Goerli 31337
+  42, // Kovan
+  1337, // Local
+];
