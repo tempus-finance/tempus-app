5eb4da9a63c2b6d14097586474e28bf92ef86c55
Lazar <Lazar__93@hotmail.com>
2021-07-08 13:59:23 +0200

    Add Material-UI library and replace some of the UI elements with library components

--- packages/tempus-client/src/index.tsx
+++ packages/tempus-client/src/index.tsx
@@ -2,17 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Web3ReactProvider } from '@web3-react/core';
 
+import { createTheme, ThemeProvider } from '@material-ui/core';
+
 import App from './components/app/App';
 
 import getLibrary from './util/get-library';
 
 import './index.scss';
 
+// Use theme object to modify colors, fonts, etc...
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#066d59',
+      dark: '#066d59',
+      light: 'ffdf99',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#ada4fd',
+      contrastText: '#ffffff',
+    },
+    divider: '#ffffff',
+  },
+  typography: {
+    fontFamily: 'Aeonik',
+  },
+});
+console.log(theme);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <App />
-    </Web3ReactProvider>
+    <ThemeProvider theme={theme}>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <App />
+      </Web3ReactProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
