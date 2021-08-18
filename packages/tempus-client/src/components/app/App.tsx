3e6e89606ceb0126ee78eb96a4e47df507540c7a
Daniele Damiani <danieledamianiuk@gmail.com>
2021-08-18 14:19:51 +0100

    Add Sidebar

--- packages/tempus-client/src/components/app/App.tsx
+++ packages/tempus-client/src/components/app/App.tsx
@@ -1,18 +1,33 @@
 import { FC, useCallback, useState } from 'react';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import IconButton from '@material-ui/core/IconButton';
 import Header from '../header/header';
 import Landing from '../landing/landing';
 import DashboardManager from '../dashboard/dashboard-manager';
+import Sidebar from '../sidebar/sidebar';
+import { DashboardRow } from '../../interfaces';
 
 import './App.scss';
 
 const App: FC = (): JSX.Element => {
   const [showDashboard, setShowDashboard] = useState<boolean>(false);
+  const [selectedRow, setSelectedRow] = useState<DashboardRow | null>(null);
   const [activeLink, setActiveLink] = useState<string>('');
+  const [openSidebar, setOpenSidebar] = useState<boolean>(false);
+
+  const onClose = useCallback(() => {
+    setOpenSidebar(false);
+  }, [setOpenSidebar]);
+
+  const onOpenSidebar = useCallback(() => {
+    setOpenSidebar(true);
+  }, [setOpenSidebar]);
 
   const showDashboardHandler = useCallback(() => {
     setShowDashboard(true);
     setActiveLink('DASHBOARD');
-  }, [setActiveLink, setShowDashboard]);
+    setOpenSidebar(true);
+  }, [setActiveLink, setShowDashboard, setOpenSidebar]);
 
   const showLandingHandler = useCallback(() => {
     setShowDashboard(false);
@@ -20,11 +35,19 @@ const App: FC = (): JSX.Element => {
   }, [setActiveLink, setShowDashboard]);
 
   return (
-    <div className="tf__app__container">
-      <Header active={activeLink} onLogoClick={showLandingHandler} onDashboardClick={showDashboardHandler} />
-      {!showDashboard && <Landing />}
-      {showDashboard && <DashboardManager />}
-    </div>
+    <>
+      <div className="tf__sidebar__handler">
+        <IconButton onClick={onOpenSidebar}>
+          <ChevronRightIcon />
+        </IconButton>
+      </div>
+      <div className="tf__app__container">
+        <Header active={activeLink} onLogoClick={showLandingHandler} onDashboardClick={showDashboardHandler} />
+        {!showDashboard && <Landing />}
+        {showDashboard && <DashboardManager selectedRow={selectedRow} onRowSelected={setSelectedRow} />}
+      </div>
+      <Sidebar open={openSidebar} content={selectedRow} onClose={onClose} />
+    </>
   );
 };
 
