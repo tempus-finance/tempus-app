d5a6466511dd579ce16843a0eceea1fe13ab35f6
Lazar Mitic <Lazar__93@hotmail.com>
2021-08-18 09:50:34 +0200

    Add loading indicator in the UI

--- packages/tempus-client/src/components/grids/transactions/transactions.tsx
+++ packages/tempus-client/src/components/grids/transactions/transactions.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Typography } from '@material-ui/core';
+import { CircularProgress, Typography } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import getTransactionsDataAdapter from '../../../adapters/getTransactionsDataAdapter';
 import { Transaction } from '../../../interfaces';
@@ -8,6 +8,7 @@ import transactionsColumnDefinitions from './transactionsColumnDefinitions';
 import './transactions.scss';
 
 const Transactions = (): JSX.Element => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
@@ -16,14 +17,21 @@ const Transactions = (): JSX.Element => {
 
       const data = await dataAdapter.generateData();
       setTransactions(data);
+      setLoading(false);
     };
     fetchTransactions();
+    setLoading(true);
   }, []);
 
   return (
     <div className="tf-transactions">
       <Typography>Transactions</Typography>
       <div className="tf-transactions-grid">
+        {loading && (
+          <div className="tf-transactions-loading-overlay">
+            <CircularProgress size={48} />
+          </div>
+        )}
         <DataGrid rows={transactions} columns={transactionsColumnDefinitions} autoHeight={true} hideFooter={true} />
       </div>
     </div>
