4bb20d10b3323bfd2919680a4cc824fb4d89c21a
Lazar Mitic <Lazar__93@hotmail.com>
2021-08-19 13:22:50 +0200

    Properly parse decimal points in currency parser (#112)

--- packages/tempus-client/src/components/currencyInput/currencyParser.test.ts
+++ packages/tempus-client/src/components/currencyInput/currencyParser.test.ts
@@ -4,6 +4,7 @@ describe('formatValueToCurrency()', () => {
   [
     { value: 0.237, expected: '0.237' },
     { value: '0.88', expected: '0.88' },
+    { value: '0.001', expected: '0.001' },
     { value: 1, expected: '1' },
     { value: 1.0, expected: '1' },
     { value: '1.0', expected: '1.0' },
@@ -11,6 +12,7 @@ describe('formatValueToCurrency()', () => {
     { value: 1000, expected: '1,000' },
     { value: '2000', expected: '2,000' },
     { value: 1234.5678, expected: '1,234.5678' },
+    { value: 1234.005678, expected: '1,234.005678' },
   ].forEach(({ value, expected }) => {
     test(`when value is '${value}' the result should be '${expected}' `, () => {
       const result = formatValueToCurrency(value);
