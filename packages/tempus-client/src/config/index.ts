90a3f1926c4067c9dfedc104752676f4131d9fa9
Lazar Mitic <Lazar__93@hotmail.com>
2021-08-31 10:17:23 +0200

    Include swap events in transactions table (#140)

--- packages/tempus-client/src/config/index.ts
+++ packages/tempus-client/src/config/index.ts
@@ -3,27 +3,16 @@ import { Config } from '../interfaces';
 // Ideally we should have only one contract address here (factory contract) that we can query to get additional contract addresses.
 // Waiting for backend team for factory contract - for now, we need to store all relevant contract addresses in this config.
 const config: Config = {
-  tokens: [
-    { ticker: 'AAVE', address: '' },
-    { ticker: 'COMP', address: '' },
-    { ticker: 'DAI', address: '' },
-    { ticker: 'ETH', address: '' }, // TBD
-    { ticker: 'TUSD', address: '' },
-    { ticker: 'USDC', address: '' },
-  ],
-  protocols: [
-    { name: 'aave', address: '' }, // TBD
-    { name: 'compound', address: '' }, // TBD
-    { name: 'lido', address: '' }, // TBD
-  ],
   tempusPools: [
     {
       address: '',
+      ammAddress: '',
       backingToken: '',
     },
   ],
   statisticsContract: '',
   tempusControllerContract: '',
+  vaultContract: '',
   networkUrl: '',
 };
 
--- packages/tempus-client/src/interfaces/Protocol.ts
+++ packages/tempus-client/src/interfaces/Protocol.ts
@@ -1,6 +1,1 @@
 export type ProtocolName = 'aave' | 'compound' | 'lido';
-
-export type Protocol = {
-  name: ProtocolName;
-  address: string;
-};
