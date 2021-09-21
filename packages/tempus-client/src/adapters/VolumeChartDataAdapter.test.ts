b7cce349756db4b2f0e9860f9ecdd11feb98de1d
Lazar <Lazar__93@hotmail.com>
2021-09-21 15:05:54 +0200

    Fix value increase calculations for Volume Chart

--- packages/tempus-client/src/adapters/VolumeChartDataAdapter.test.ts
+++ packages/tempus-client/src/adapters/VolumeChartDataAdapter.test.ts
@@ -1,50 +1,154 @@
-import VolumeChartDataAdapter from './VolumeChartDataAdapter';
+import * as ejs from 'ethers';
+import { SECONDS_IN_A_DAY } from '../constants';
 
-jest.mock('ethers');
-const { Contract } = jest.requireMock('ethers');
+import VolumeChartDataAdapter from './VolumeChartDataAdapter';
 
 jest.mock('@ethersproject/providers');
 const { JsonRpcProvider } = jest.requireMock('@ethersproject/providers');
 
-// Adding any here so that I can mock private functions - there doesn't seem to by any other way to do so.
-let instance: any;
-let spyFetchData: any;
-let spyGetChartDataPoint: any;
-let mockProvider = new JsonRpcProvider();
+let instance: VolumeChartDataAdapter;
 
 describe('generateChartData()', () => {
   beforeEach(() => {
     jest.clearAllMocks();
 
-    Contract.mockImplementation(() => {
-      return {};
+    let mockProvider = new JsonRpcProvider();
+
+    jest.spyOn(ejs as any, 'Contract').mockReturnValue({
+      getPoolId: jest.fn(),
+      queryFilter: jest.fn(),
+      filters: {
+        Deposited: jest.fn(),
+        Redeemed: jest.fn(),
+        Swap: jest.fn(),
+      },
     });
 
-    instance = new VolumeChartDataAdapter();
-    spyFetchData = jest.spyOn(instance, 'fetchData').mockImplementation(() => {});
-    spyGetChartDataPoint = jest.spyOn(instance, 'getChartDataPoint').mockImplementation(() => {
+    let mockGetBlock = jest.fn();
+    for (let i = 0; i < 30; i++) {
+      mockGetBlock = mockGetBlock.mockResolvedValueOnce({
+        number: i,
+        timestamp: Math.floor(new Date().getTime() / 1000) - SECONDS_IN_A_DAY * (i / 2),
+      });
+    }
+
+    const mockGetTempusControllerService = jest.fn().mockImplementation(() => {
       return {
-        value: 100,
-        date: new Date(1629180909484),
-        valueIncrease: 50,
+        getDepositedEvents: jest.fn().mockResolvedValue([
+          {
+            name: 'd-event-1',
+            blockNumber: 0,
+            args: {
+              depositor: 'depositor-1',
+              pool: 'pool-1',
+              backingTokenValue: ejs.utils.parseEther('20'),
+            },
+            getBlock: mockGetBlock,
+          },
+          {
+            name: 'd-event-2',
+            blockNumber: 1,
+            args: {
+              depositor: 'depositor-1',
+              pool: 'pool-1',
+              backingTokenValue: ejs.utils.parseEther('25'),
+            },
+            getBlock: mockGetBlock,
+          },
+        ]),
+        getRedeemedEvents: jest.fn().mockResolvedValue([
+          {
+            name: 'r-event-1',
+            blockNumber: 5,
+            args: {
+              redeemer: 'redeemer-1',
+              pool: 'pool-1',
+              backingTokenValue: ejs.utils.parseEther('26'),
+            },
+            getBlock: mockGetBlock,
+          },
+          {
+            name: 'r-event-2',
+            blockNumber: 2,
+            args: {
+              redeemer: 'redeemer-2',
+              pool: 'pool-1',
+              backingTokenValue: ejs.utils.parseEther('500'),
+            },
+            getBlock: mockGetBlock,
+          },
+        ]),
       };
     });
 
-    instance.init(mockProvider);
+    const mockGetVaultService = jest.fn().mockImplementation(() => {
+      return {
+        getSwapEvents: jest.fn().mockResolvedValue([
+          {
+            name: 's-event-1',
+            blockNumber: 3,
+            args: {
+              tokenIn: 'principal-address',
+              amountIn: ejs.utils.parseEther('5'),
+            },
+            getBlock: mockGetBlock,
+          },
+          {
+            name: 's-event-2',
+            blockNumber: 3,
+            args: {
+              tokenIn: 'principal-address',
+              amountIn: ejs.utils.parseEther('3'),
+            },
+            getBlock: mockGetBlock,
+          },
+        ]),
+      };
+    });
+
+    const mockGetTempusAMMService = jest.fn().mockImplementation(() => {
+      return {
+        getTempusPoolAddressFromId: jest.fn().mockResolvedValue('pool-1'),
+      };
+    });
+
+    const mockGetTempusPoolService = jest.fn().mockImplementation(() => {
+      return {
+        getBackingTokenTicker: jest.fn().mockResolvedValue('dai'),
+        getPrincipalTokenAddress: jest.fn().mockResolvedValue('principal-address'),
+      };
+    });
+
+    const mockGetStatisticsService = jest.fn().mockImplementation(() => {
+      return {
+        getRate: jest.fn().mockResolvedValue(ejs.ethers.utils.parseEther('2')),
+      };
+    });
+
+    instance = new VolumeChartDataAdapter();
+    instance.init({
+      signerOrProvider: mockProvider,
+      statisticsService: mockGetStatisticsService(),
+      tempusAMMService: mockGetTempusAMMService(),
+      tempusControllerService: mockGetTempusControllerService(),
+      tempusPoolService: mockGetTempusPoolService(),
+      vaultService: mockGetVaultService(),
+    });
   });
 
   test('it returns an array of data points for past 30 days', async () => {
     const chartData = await instance.generateChartData();
 
-    expect(spyFetchData).toHaveBeenCalledTimes(1);
-    expect(spyGetChartDataPoint).toHaveBeenCalledTimes(30);
-
     expect(chartData.length).toBe(30);
-    expect(chartData[0].value).toBe(100);
-    expect(chartData[0].date).toBeInstanceOf(Date);
-    expect(chartData[0].date.getFullYear()).toBe(2021);
-    expect(chartData[0].date.getMonth()).toBe(7);
-    expect(chartData[0].date.getUTCDate()).toBe(17);
-    expect(chartData[0].valueIncrease).toBe(50);
+
+    expect(chartData[29].value).toBe('90.0');
+    expect(chartData[28].value).toBe('1016.0');
+    expect(chartData[27].value).toBe('52.0');
+    expect(chartData[26].value).toBe('0.0');
+
+    expect(Number(chartData[29].valueIncrease).toFixed(2)).toBe('-91.14');
+    expect(Number(chartData[28].valueIncrease).toFixed(2)).toBe('1853.85');
+    expect(Number(chartData[27].valueIncrease).toFixed(2)).toBe('0.00');
+    expect(Number(chartData[26].valueIncrease).toFixed(2)).toBe('0.00');
   });
 });
--- packages/tempus-client/src/adapters/getVolumeChartDataAdapter.ts
+++ packages/tempus-client/src/adapters/getVolumeChartDataAdapter.ts
@@ -1,5 +1,10 @@
 import { JsonRpcSigner, JsonRpcProvider } from '@ethersproject/providers';
 import getDefaultProvider from '../services/getDefaultProvider';
+import getStatisticsService from '../services/getStatisticsService';
+import getTempusAMMService from '../services/getTempusAMMService';
+import getTempusControllerService from '../services/getTempusControllerService';
+import getTempusPoolService from '../services/getTempusPoolService';
+import getVaultService from '../services/getVaultService';
 import VolumeChartDataAdapter from './VolumeChartDataAdapter';
 
 let volumeChartDataAdapter: VolumeChartDataAdapter;
@@ -8,12 +13,22 @@ const getVolumeChartDataAdapter = (signerOrProvider?: JsonRpcSigner | JsonRpcPro
     volumeChartDataAdapter = new VolumeChartDataAdapter();
     volumeChartDataAdapter.init({
       signerOrProvider: getDefaultProvider(),
+      statisticsService: getStatisticsService(),
+      tempusAMMService: getTempusAMMService(),
+      tempusControllerService: getTempusControllerService(),
+      tempusPoolService: getTempusPoolService(),
+      vaultService: getVaultService(),
     });
   }
 
   if (signerOrProvider) {
     volumeChartDataAdapter.init({
       signerOrProvider: signerOrProvider,
+      statisticsService: getStatisticsService(signerOrProvider),
+      tempusAMMService: getTempusAMMService(signerOrProvider),
+      tempusControllerService: getTempusControllerService(signerOrProvider),
+      tempusPoolService: getTempusPoolService(signerOrProvider),
+      vaultService: getVaultService(signerOrProvider),
     });
   }
 
