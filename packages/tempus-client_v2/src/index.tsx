f901a0a4a1086c22c6041ced58de056af5d14e3a
Daniele Damiani <danieledamianiuk@gmail.com>
2021-10-27 17:08:45 +0100

    Scaffolding Tempus Client V2

---
+++ packages/tempus-client_v2/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Tempus Client V2</div>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
