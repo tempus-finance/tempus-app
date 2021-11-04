420cba7eaa32e77ecfb158f8ae3bcbe270034398
Lazar Mitic <Lazar__93@hotmail.com>
2021-11-04 07:31:20 +0100

    Add Provide Liquidity view

--- packages/tempus-client_v2/src/utils/weiMath.ts
+++ packages/tempus-client_v2/src/utils/weiMath.ts
@@ -4,11 +4,11 @@ import { ONE_ETH_IN_WEI } from '../constants';
 const weiInEth = BigNumber.from(ONE_ETH_IN_WEI);
 
 export function mul18f(a: BigNumber, b: BigNumber, precision?: number): BigNumber {
-  const bigNumberPrecision = precision ? BigNumber.from(Math.pow(10, precision)) : weiInEth;
+  const bigNumberPrecision = precision ? BigNumber.from(Math.pow(10, precision).toString()) : weiInEth;
   return a.mul(b).div(bigNumberPrecision);
 }
 
 export function div18f(a: BigNumber, b: BigNumber, precision?: number): BigNumber {
-  const bigNumberPrecision = precision ? BigNumber.from(Math.pow(10, precision)) : weiInEth;
+  const bigNumberPrecision = precision ? BigNumber.from(Math.pow(10, precision).toString()) : weiInEth;
   return a.mul(bigNumberPrecision).div(b);
 }
