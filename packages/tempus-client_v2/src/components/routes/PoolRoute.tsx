2c1fbd955922a4ee087cdc10661903e4952fb969
Lazar Mitic <Lazar__93@hotmail.com>
2021-12-08 11:58:54 +0100

    Wait for pool required data to load before showing pool UI (#581)

--- packages/tempus-client_v2/src/components/routes/PoolRoute.tsx
+++ packages/tempus-client_v2/src/components/routes/PoolRoute.tsx
@@ -1,13 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useHookstate } from '@hookstate/core';
-import { selectedPoolState } from '../../state/PoolDataState';
+import { BigNumber } from 'ethers';
+import { Downgraded, useHookstate } from '@hookstate/core';
+import { dynamicPoolDataState, selectedPoolState } from '../../state/PoolDataState';
 import Operations from '../operations/Operations';
 
 const PoolRoute = () => {
   const selectedPool = useHookstate(selectedPoolState);
+  const dynamicPoolData = useHookstate(dynamicPoolDataState);
+
+  const [poolShareBalanceLoaded, setPoolShareBalanceLoaded] = useState<boolean>(false);
 
   const params = useParams();
+
+  let poolShareBalance: {
+    principals: BigNumber | null;
+    yields: BigNumber | null;
+  } = {
+    principals: null,
+    yields: null,
+  };
+  if (params.poolAddress) {
+    poolShareBalance = dynamicPoolData[params.poolAddress].poolShareBalance.attach(Downgraded).get();
+  }
+
+  useEffect(() => {
+    if (params.poolAddress) {
+      if (poolShareBalance.principals && poolShareBalance.yields) {
+        setPoolShareBalanceLoaded(true);
+      }
+    }
+  }, [params.poolAddress, poolShareBalance.principals, poolShareBalance.yields]);
 
   useEffect(() => {
     if (params.poolAddress) {
@@ -18,6 +41,12 @@ const PoolRoute = () => {
   }, [params.poolAddress]);
 
   if (!selectedPool.get()) {
+    return null;
+  }
+
+  // Do not show pool UI before all required data is loaded
+  // Triggered when user opens direct pool link
+  if (!poolShareBalanceLoaded) {
     return null;
   }
 
