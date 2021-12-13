a3119766e0eba87f1f432210987455a24f8a8b87
Daniele Damiani <danieledamianiuk@gmail.com>
2021-12-13 12:08:31 +0000

    Update config for mainnet (#608)

---
+++ packages/tempus-client_v2/README.md
@@ -0,0 +1,9 @@
+## Tempus client v2
+
+### Before start
+
+Copy the content of `.env.template` into `.env` replacing the placeholder values with the actual ones
+
+### Start
+
+`npm start`
--- packages/tempus-client_v2/src/config/config.ts
+++ packages/tempus-client_v2/src/config/config.ts
@@ -1,19 +1,59 @@
 import { Config } from '../interfaces/Config';
 
+// const GOERLI_ALCHEMY_KEY = process.env.REACT_APP_GOERLI_ALCHEMY_KEY || '';
+// const GOERLI_ALCHEMY_CONFIG = {
+//   networkUrl: 'https://eth-goerli.alchemyapi.io/v2/${GOERLI_ALCHEMY_KEY}',
+//   networkName: 'goerli',
+//   alchemyKey: GOERLI_ALCHEMY_KEY,
+// } as Config;
+
+const MAINNET_ALCHEMY_KEY = process.env.REACT_APP_MAINNET_ALCHEMY_KEY || '';
+const MAINNET_ALCHEMY_CONFIG = {
+  networkUrl: `https://eth-mainnet.alchemyapi.io/v2/${MAINNET_ALCHEMY_KEY}`,
+  networkName: 'homestead',
+  alchemyKey: MAINNET_ALCHEMY_KEY,
+} as Config;
+
 // Ideally we should have only one contract address here (factory contract) that we can query to get additional contract addresses.
 // Waiting for backend team for factory contract - for now, we need to store all relevant contract addresses in this config.
+
+// TODO network config should be set by the provider
 const config: Config = {
+  ...MAINNET_ALCHEMY_CONFIG,
   tempusPools: [
     {
-      address: '0x5805D67782F23e193F4818A0B1e27fbb47086638',
-      poolId: '0xc66a272890ef3c811afe718bceec0cad5e2cb286000200000000000000000043',
-      ammAddress: '0xc66A272890Ef3C811afe718BcEec0cAD5E2Cb286',
-      principalsAddress: '0x27d4F1b6999379ee9e5BE0D44fdF4b372Eb80854',
-      yieldsAddress: '0x89dd29CaF097E706aA4D16D449d48496CeCb317B',
-      yieldBearingTokenAddress: '0x209b1C2B038ef377f6f86d33C5Ca94d10ed9C89d',
+      address: '0x6320E6844EEEa57343d5Ca47D3166822Ec78b116',
+      poolId: '0x7004797ad44897f90401609c075e2f082be9d8be000200000000000000000000',
+      ammAddress: '0x7004797ad44897f90401609C075E2F082be9D8Be',
+      principalsAddress: '0x5A5cBa78Ae5ebd7142D0B8C796565388BA4B5732',
+      yieldsAddress: '0x1f436309fBfB405192Acdc80d5C284De45e520EB',
+      yieldBearingTokenAddress: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
       backingTokenAddress: '0x0000000000000000000000000000000000000000',
-      startDate: 1638382591000,
-      maturityDate: 1651363200000,
+      startDate: 1639157275000,
+      maturityDate: 1648742400000,
+      protocol: 'lido',
+      backingToken: 'ETH',
+      yieldBearingToken: 'stETH',
+      spotPrice: '2',
+      decimalsForUI: 4,
+      tokenPrecision: {
+        backingToken: 18,
+        lpTokens: 18,
+        principals: 18,
+        yieldBearingToken: 18,
+        yields: 18,
+      },
+    },
+    {
+      address: '0x0697B0a2cBb1F947f51a9845b715E9eAb3f89B4F',
+      poolId: '0x200e41be620928351f98da8031baeb7bd401a129000200000000000000000001',
+      ammAddress: '0x200e41BE620928351F98Da8031BAEB7BD401a129',
+      principalsAddress: '0x2C4AC125044e853F0f6d66b95365CBBa204fFCFD',
+      yieldsAddress: '0xfFaCF0b02851e440FA207Ea2f9AfDF7FfE0bE095',
+      yieldBearingTokenAddress: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84',
+      backingTokenAddress: '0x0000000000000000000000000000000000000000',
+      startDate: 1639158404000,
+      maturityDate: 1661875200000,
       protocol: 'lido',
       backingToken: 'ETH',
       yieldBearingToken: 'stETH',
@@ -28,12 +68,10 @@ const config: Config = {
       },
     },
   ],
-  statisticsContract: '0x01fF82791D8414826ec7390dfE7902703F632F5C',
-  tempusControllerContract: '0x8E8f7485f0d4b8b27A0660286d2eEA04e9e1caB9',
-  vaultContract: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
-  networkUrl: 'https://eth-goerli.alchemyapi.io/v2/RUE8LXWaELu4_nyMgfK99eX3DWoxLtDw',
-  networkName: 'goerli',
-  alchemyKey: 'Hz57g3uvMUx9K9mCmAODf75Wba8N2Fjp',
-  lidoOracle: '0x24d8451BC07e7aF4Ba94F69aCDD9ad3c6579D9FB',
+  statisticsContract: '0xf7247e16d6cD0A64d0C210f150fD356AcF26E89C',
+  tempusControllerContract: '0xdB5fD0678eED82246b599da6BC36B56157E4beD8',
+  vaultContract: '0x6f419298Ad53f82BA8dFFa9B34F9C7888b43BB13',
+  lidoOracle: '0x442af784a788a5bd6f42a01ebe9f287a871243fb',
 };
+
 export default config;
