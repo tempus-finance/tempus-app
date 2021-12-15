0bd24739a452585d5679bcc9fedcee644e541dbc
Lazar Mitic <Lazar__93@hotmail.com>
2021-12-15 11:49:18 +0100

    remove comments

--- packages/tempus-client_v2/src/providers/getBalanceProvider.ts
+++ packages/tempus-client_v2/src/providers/getBalanceProvider.ts
@@ -1,7 +1,5 @@
 import UserBalanceProvider, { UserBalanceProviderParams } from './balanceProvider';
 
-// Key - User wallet address
-// Value - Provider instance
 const getUserBalanceProvider = (params: UserBalanceProviderParams): UserBalanceProvider => {
   return new UserBalanceProvider(params);
 };
