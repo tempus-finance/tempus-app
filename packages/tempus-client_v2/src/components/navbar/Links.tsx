a08f5d465ccc2ba30a353b25c1cd3431cdae72fc
Lazar Mitic <Lazar__93@hotmail.com>
2022-02-04 11:13:08 +0100

    Remove dashboard link from navbar (#887)

--- packages/tempus-client_v2/src/components/navbar/Links.tsx
+++ packages/tempus-client_v2/src/components/navbar/Links.tsx
@@ -1,28 +1,12 @@
-import { useCallback, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { LanguageContext } from '../../context/languageContext';
-import getText from '../../localisation/getText';
 import Community from './Community';
 import Settings from './Settings';
 
 import './Links.scss';
 
 const Links = () => {
-  const { language } = useContext(LanguageContext);
-
-  const navigate = useNavigate();
-
-  const onDashboardClick = useCallback(() => {
-    navigate('/');
-  }, [navigate]);
-
-  // TODO
-  // link active state
-
   return (
     <div className="tc__navBar__links">
       <ul>
-        <li onClick={onDashboardClick}>{getText('dashboard', language)}</li>
         <Community />
         <Settings />
       </ul>
