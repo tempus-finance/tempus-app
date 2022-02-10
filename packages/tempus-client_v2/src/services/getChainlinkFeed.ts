5d37d037024111028058cfcf159af9ea9775a3e5
Lazar Mitic <Lazar__93@hotmail.com>
2022-02-10 09:11:19 +0100

    Add missing chainlink feeds

--- packages/tempus-client_v2/src/services/getChainlinkFeed.ts
+++ packages/tempus-client_v2/src/services/getChainlinkFeed.ts
@@ -9,6 +9,12 @@ const chainlinkMap: { [key in Chain]: { [pair: string]: string } } = {
   },
   fantom: {
     'dai-usd': '0x91d5DEFAFfE2854C7D02F50c80FA1fdc8A721e52',
+    'usdc-usd': '0x2553f4eeb82d5A26427b8d1106C51499CBa5D99c',
+    'yfi-usd': '0x9B25eC3d6acfF665DfbbFD68B3C1D896E067F0ae',
+    'usdt-usd': '0xF64b636c5dFe1d3555A847341cDC449f612307d0',
+    'ftm-usd': '0xf4766552D15AE4d256Ad41B6cf2933482B0680dc',
+    'weth-usd': '0x11DdD3d147E5b83D01cee7070027092397d63658',
+    'wbtc-usd': '0x8e94C22142F4A64b99022ccDd994f4e9EC86E4B4',
   },
 };
 
