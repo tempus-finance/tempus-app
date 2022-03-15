27b71094c46d8374fb765d10d3ec0350fc4d9ada
Daniele Damiani <danieledamianiuk@gmail.com>
2022-03-15 09:41:48 +0000

    Scaffold 'tempus-core-services' package

---
+++ packages/tempus-core-services/src/index.ts
@@ -0,0 +1,16 @@
+export type {
+  Chain,
+  ChainConfig,
+  Config,
+  PoolShares,
+  ProtocolDisplayName,
+  ProtocolName,
+  Ticker,
+  TokenPrecision,
+  TokenTypePrecision,
+  YearnData,
+  YearnDataApy,
+  YearnDataToken,
+} from './interfaces';
+
+export * as CONSTANTS from './constants';
