576c2f825a7c4cae13df4320667c5ffc5654932d
ShawTim <shawtim.liu@gmail.com>
2022-03-22 16:16:21 +0800

    hide the columns when unconnected but still want to hold the width (#1214)
    * hide the columns when unconnected but still want to hold the width
    
    * wrongly removed TVL column, bring it back

--- packages/tempus-client_v2/src/components/dashboard/dashboardColumnsDefinitions.tsx
+++ packages/tempus-client_v2/src/components/dashboard/dashboardColumnsDefinitions.tsx
@@ -7,45 +7,64 @@ export interface ExtraDataColumn extends Column {
   tooltip?: string;
 }
 
-export const dashboardColumnsDefinitions = (locale: Locale): ExtraDataColumn[] => [
-  {
-    name: 'token',
-    title: getText('asset', locale),
-  },
-  {
-    name: 'protocol',
-    title: getText('protocol', locale),
-    // TODO - Hide protocol icons from children that are hidden
-    getCellValue: (row: DashboardRowChild | DashboardRowParent) => {
-      if (isParentRow(row)) {
-        return row.protocols;
-      } else {
-        return row.tempusPool.protocol;
-      }
+export const dashboardColumnsDefinitions = (isConnected: boolean, locale: Locale): ExtraDataColumn[] => {
+  const defaultColumnsDefinitions = [
+    {
+      name: 'token',
+      title: getText('asset', locale),
     },
-  },
-  {
-    name: 'maturity',
-    title: getText('maturity', locale),
-  },
-  {
-    name: 'fixedAPR',
-    title: getText('fixedApr', locale),
-  },
-  // {
-  //   name: 'variableAPY',
-  //   title: getText('lpApr', locale),
-  // },
-  {
-    name: 'TVL',
-    title: getText('tvl', locale),
-  },
-  {
-    name: 'presentValue',
-    title: getText('balance', locale),
-  },
-  {
-    name: 'availableToDeposit',
-    title: getText('availableToDeposit', locale),
-  },
-];
+    {
+      name: 'protocol',
+      title: getText('protocol', locale),
+      // TODO - Hide protocol icons from children that are hidden
+      getCellValue: (row: DashboardRowChild | DashboardRowParent) => {
+        if (isParentRow(row)) {
+          return row.protocols;
+        } else {
+          return row.tempusPool.protocol;
+        }
+      },
+    },
+    {
+      name: 'maturity',
+      title: getText('maturity', locale),
+    },
+    {
+      name: 'fixedAPR',
+      title: getText('fixedApr', locale),
+    },
+    // {
+    //   name: 'variableAPY',
+    //   title: getText('lpApr', locale),
+    // },
+    {
+      name: 'TVL',
+      title: getText('tvl', locale),
+    },
+  ];
+  const connectedColumnsDefinitions = [
+    {
+      name: 'presentValue',
+      title: getText('balance', locale),
+    },
+    {
+      name: 'availableToDeposit',
+      title: getText('availableToDeposit', locale),
+    },
+  ];
+  // if unconnected, show empty column header
+  const unconnectedColumnsDefinitions = [
+    {
+      name: 'presentValue',
+      title: ' ',
+    },
+    {
+      name: 'availableToDeposit',
+      title: ' ',
+    },
+  ];
+
+  return isConnected
+    ? defaultColumnsDefinitions.concat(connectedColumnsDefinitions)
+    : defaultColumnsDefinitions.concat(unconnectedColumnsDefinitions);
+};
