2232cb327bee960cbe5539ca7e7dc53c08ccc04b
Lazar Mitic <Lazar__93@hotmail.com>
2022-03-25 14:12:40 +0100

    test

--- bundlewatch.config.js
+++
@@ -1,9 +0,0 @@
-const bundlewatchConfig = {
-  files: [
-    {
-      path: 'packages/tempus-client_v2/build/static/js/*.js',
-    },
-  ],
-  normalizeFilenames: /^.+?(\..+?)\.\w+$/,
-};
-module.exports = bundlewatchConfig;
