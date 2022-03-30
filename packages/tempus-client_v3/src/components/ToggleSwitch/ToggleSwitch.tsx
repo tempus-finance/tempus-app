9779285e3d0624b3da77f26d86e4994606d6268e
Vukašin Manojlović <iamvukasin@gmail.com>
2022-03-30 10:42:11 +0200

    Simplify toggle switch's props

--- packages/tempus-client_v3/src/components/ToggleSwitch/ToggleSwitch.tsx
+++ packages/tempus-client_v3/src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -1,22 +1,23 @@
-import { useMemo } from 'react';
+import { ChangeEventHandler, FC, useMemo } from 'react';
 import { Typography } from '../shared';
 import './toggle-switch.scss';
 
 interface ToggleSwitchProps {
   checked: boolean;
   label?: string;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
 let idCounter = 0;
 
-const ToggleSwitch = (props: ToggleSwitchProps & React.HTMLProps<HTMLInputElement>) => {
-  const { checked, label, ...checkboxProps } = props;
-  const id = useMemo(() => checkboxProps.id ?? `toggle-switch-${idCounter++}`, [checkboxProps.id]);
+const ToggleSwitch: FC<ToggleSwitchProps> = props => {
+  const { checked, label, onChange } = props;
+  const id = useMemo(() => `toggle-switch-${idCounter++}`, []);
 
   return (
     <span className="tc__toggle-switch">
       <span className="tc__toggle-switch__switch-container">
-        <input {...checkboxProps} id={id} checked={checked} type="checkbox" />
+        <input id={id} checked={checked} type="checkbox" onChange={onChange} />
         <span className={`tc__toggle-switch__thumb ${checked ? 'tc__toggle-switch__thumb-active' : ''}`}></span>
         <span
           className={`tc__toggle-switch__background ${checked ? 'tc__toggle-switch__background-active' : ''}`}
