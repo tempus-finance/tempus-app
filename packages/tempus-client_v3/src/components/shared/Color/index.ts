ebafafbfcc2bded55250dd05d7abe1104ad0e3c4
Vukašin Manojlović <iamvukasin@gmail.com>
2022-03-30 11:08:44 +0200

    Create loader for CSS variables

--- packages/tempus-client_v3/src/components/shared/Color/index.ts
+++ packages/tempus-client_v3/src/components/shared/Color/index.ts
@@ -1,13 +1,9 @@
+import loadStyleVariables from '../loadStyleVariables';
 import colorScheme from './colors.json';
 
 export const colors = colorScheme;
 
-const root = document.querySelector(':root') as HTMLElement;
-if (root) {
-  for (const [key, value] of Object.entries(colors)) {
-    root.style.setProperty(`--${key}`, value);
-  }
-}
+loadStyleVariables(colors);
 
 // Required by TS - we need at least one export or import so that TS considers this file a module
 export {};
--- packages/tempus-client_v3/src/components/shared/Shadow/index.ts
+++ packages/tempus-client_v3/src/components/shared/Shadow/index.ts
@@ -1,10 +1,6 @@
+import loadStyleVariables from '../loadStyleVariables';
 import allShadows from './shadows.json';
 
 export const shadows = allShadows;
 
-const root = document.querySelector(':root') as HTMLElement;
-if (root) {
-  for (const [key, value] of Object.entries(shadows)) {
-    root.style.setProperty(`--${key}Shadow`, value);
-  }
-}
+loadStyleVariables(shadows, 'Shadow');
