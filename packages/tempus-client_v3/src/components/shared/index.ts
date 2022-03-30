9c3f9ca7792a4afe0bea76f4dcf16f4e284007b4
Lazar Mitic <Lazar__93@hotmail.com>
2022-03-30 10:04:29 +0200

    Add Loading component

--- packages/tempus-client_v3/src/components/shared/index.ts
+++ packages/tempus-client_v3/src/components/shared/index.ts
@@ -1,7 +1,9 @@
 import Typography from './Typography';
 import Icon from './Icon';
+import Loading from './Loading';
 
 export * from './Color';
 export * from './Icon';
+export * from './Loading';
 
-export { Icon, Typography };
+export { Icon, Typography, Loading };
