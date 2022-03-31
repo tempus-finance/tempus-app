b3148a93d55af51a864e7e71601b1bd5d484238e
Daniele Damiani <danieledamianiuk@gmail.com>
2022-03-31 11:00:47 +0100

    Add Button specs

---
+++ packages/tempus-client_v3/src/components/shared/Button/Button.spec.jsx
@@ -0,0 +1,34 @@
+import { fireEvent, render } from '@testing-library/react';
+import Button from './Button';
+
+const mockOnClick = jest.fn();
+
+const defaultProps = {
+  title: 'my title',
+  onClick: mockOnClick,
+};
+
+const subject = props => render(<Button {...props}>My Button</Button>);
+
+describe('Button', () => {
+  it('renders a button with a title attribute', () => {
+    const { queryByTitle, queryByText } = subject(defaultProps);
+
+    const actualText = queryByText('My Button');
+    const actualLabel = queryByTitle('my title');
+
+    expect(actualText).not.toBeNull();
+    expect(actualLabel).not.toBeNull();
+
+    expect(actualText).toMatchSnapshot();
+  });
+
+  it('calls `onClick` when the button is clicked', () => {
+    const { getByRole } = subject(defaultProps);
+
+    const button = getByRole('button');
+    fireEvent.click(button);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
+});
