eab1be2e55e5413b3533cf4cddec75494a2e7d46
Daniele Damiani <danieledamianiuk@gmail.com>
2022-03-31 10:02:33 +0100

    Add Checkbox and ToggleSwitch stories

--- packages/tempus-client_v3/src/index.tsx
+++ packages/tempus-client_v3/src/index.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import isMobile from 'is-mobile';
 import reportWebVitals from './reportWebVitals';
-import { Checkbox, ToggleSwitch, Typography } from './components/shared';
+import { Typography } from './components/shared';
 
 // Creates CSS variables for all color constants
 import './components/shared/Colors';
@@ -20,33 +20,6 @@ try {
   console.log(`Current version: ${release.releaseVersion}`);
 } catch (e) {}
 
-const CheckboxDemo = () => {
-  const [selected1, setSelected1] = useState(false);
-  const [selected2, setSelected2] = useState(false);
-  const handleChange1 = useCallback(() => {
-    setSelected1(!selected1);
-  }, [selected1]);
-  const handleChange2 = useCallback(() => {
-    setSelected2(!selected2);
-  }, [selected2]);
-
-  return (
-    <>
-      <Checkbox checked={selected1} onChange={handleChange1} />
-      <Checkbox checked={selected2} label="Label" onChange={handleChange2} />
-    </>
-  );
-};
-
-const ToggleSwitchDemo = () => {
-  const [selected, setSelected] = useState(false);
-  const handleChange = useCallback(() => {
-    setSelected(!selected);
-  }, [selected]);
-
-  return <ToggleSwitch checked={selected} label="Label" onChange={handleChange} />;
-};
-
 if (mobile) {
   ReactDOM.render(
     <React.StrictMode>
@@ -59,8 +32,6 @@ if (mobile) {
     <React.StrictMode>
       <BrowserRouter>
         <Typography variant="body-primary">Here the app</Typography>
-        <CheckboxDemo />
-        <ToggleSwitchDemo />
       </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root'),
