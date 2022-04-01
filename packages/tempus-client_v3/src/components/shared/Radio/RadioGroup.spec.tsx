9e86f4785b15fb665218cdff3ea20139b9747919
Vukašin Manojlović <iamvukasin@gmail.com>
2022-04-01 17:50:03 +0200

    Add basic spec for radio button and radio group

---
+++ packages/tempus-client_v3/src/components/shared/Radio/RadioGroup.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import Radio from './Radio';
+import RadioGroup, { RadioGroupProps } from './RadioGroup';
+
+const subject = (props: RadioGroupProps) =>
+  render(
+    <RadioGroup {...props}>
+      <Radio value="item1" label="Item #1" />
+      <Radio value="item2" label="Item #2" />
+      <Radio value="item3" label="Item #3" />
+    </RadioGroup>,
+  );
+
+describe('RadioGroup', () => {
+  it('renders three radio buttons with labels', () => {
+    const { getAllByRole, getAllByText } = subject({ value: 'item3' });
+
+    const radioButtons = getAllByRole('radio');
+    const labels = getAllByText(/Item #\d/);
+
+    expect(radioButtons).not.toBeNull();
+    expect(radioButtons).toHaveLength(3);
+    expect(radioButtons[0]).not.toBeChecked();
+    expect(radioButtons[1]).not.toBeChecked();
+    expect(radioButtons[2]).toBeChecked();
+
+    expect(labels).not.toBeNull();
+    expect(labels).toHaveLength(3);
+
+    labels.forEach((label, index) => expect(label).toHaveTextContent(`Item #${index + 1}`));
+  });
+});
