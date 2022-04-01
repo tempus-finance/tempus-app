07e88a15210ad749d842fc5c6921c2dcd4fc5ad1
Lazar Mitic <Lazar__93@hotmail.com>
2022-04-01 09:44:48 +0200

    Use button instead of div

--- packages/tempus-client_v3/src/components/shared/IconButton/IconButton.tsx
+++ packages/tempus-client_v3/src/components/shared/IconButton/IconButton.tsx
@@ -1,20 +1,22 @@
 import { FC, memo } from 'react';
 import { colors } from '../Colors';
-import Icon, { IconType } from '../Icon/Icon';
+import Icon, { IconType } from '../Icon';
+import Button from '../Button';
 import './IconButton.scss';
 
 interface IconButtonProps {
   type: IconType;
+  onClick: () => void;
   selected?: boolean;
 }
 
 const IconButton: FC<IconButtonProps> = props => {
-  const { type, selected = false } = props;
+  const { type, selected = false, onClick } = props;
 
   return (
-    <div className={selected ? 'tc__iconButton__selected' : 'tc__iconButton__deselected'}>
+    <Button className={selected ? 'tc__iconButton__selected' : 'tc__iconButton__deselected'} onClick={onClick}>
       <Icon type={type} color={selected ? colors.iconButtonSelectedIcon : colors.iconButtonIcon} />
-    </div>
+    </Button>
   );
 };
 export default memo(IconButton);
