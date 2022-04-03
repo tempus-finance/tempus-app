6a504234162d23d3e04c5c4247221c7da0f7d8bc
ShawTim <shawtim.liu@gmail.com>
2022-04-04 03:40:51 +0800

    snapshot test for SwitcherButton

---
+++ packages/tempus-client_v3/src/components/shared/SwitcherButton/SwitcherButton.spec.jsx
@@ -0,0 +1,51 @@
+import { fireEvent, render } from '@testing-library/react';
+import SwitcherButton from './SwitcherButton';
+
+const mockOnClick = jest.fn();
+
+const defaultProps = {
+  logoType: 'wallet-metamask',
+  label: 'button label',
+  title: 'button title',
+  selected: false,
+  onClick: mockOnClick,
+};
+
+const subject = props => render(<SwitcherButton {...props} />);
+
+describe('SwitcherButton', () => {
+  it('renders a button with a title attribute', () => {
+    const { getByRole, queryByTitle, queryByText } = subject(defaultProps);
+
+    const button = getByRole('button');
+    const actualText = queryByText('button label');
+    const actualLabel = queryByTitle('button title');
+
+    expect(actualText).not.toBeNull();
+    expect(actualLabel).not.toBeNull();
+
+    expect(button).toMatchSnapshot();
+  });
+
+  it('renders a selected button', () => {
+    const { getByRole, queryByTitle, queryByText } = subject({ ...defaultProps, selected: true });
+
+    const button = getByRole('button');
+    const actualText = queryByText('button label');
+    const actualLabel = queryByTitle('button title');
+
+    expect(actualText).not.toBeNull();
+    expect(actualLabel).not.toBeNull();
+
+    expect(button).toMatchSnapshot();
+  });
+
+  it('calls `onClick` when the button is clicked', () => {
+    const { getByRole } = subject(defaultProps);
+
+    const button = getByRole('button');
+    fireEvent.click(button);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
+});
