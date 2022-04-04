fc72633176eaf5daff42930573cec471601a50ab
Lazar Mitic <Lazar__93@hotmail.com>
2022-04-04 10:59:04 +0200

    Remove tempus-client_v2 codebase

--- packages/tempus-client_v2/src/components/app/App.test.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-describe('mock test', () => {
-  it('should alway pass', () => {
-    expect(true).toBe(true);
-  });
-});
--- packages/tempus-client_v2/src/components/dashboard/providers/gridBalanceProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { DataTypeProvider } from '@devexpress/dx-react-grid';
-import BalanceFormatter from './balanceFormatter';
-
-const BalanceProvider = (props: any) => <DataTypeProvider formatterComponent={BalanceFormatter} {...props} />;
-
-export default BalanceProvider;
