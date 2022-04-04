5385b6f82ba4db58817b6e98f04b739112495d51
irinelT <irinel@tempus.finance>
2022-04-04 15:20:44 +0200

    Experimental CI change 38.

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-    reporter: [['junit', { outputFile: process.env.CI ? '/root/project/test-results/results.xml' : './test-results/results.xml' }]],
+    reporter: [['junit', { outputFile: './test-results/results.xml' }]],
     use: {
         screenshot: 'on',
         video: 'on',
