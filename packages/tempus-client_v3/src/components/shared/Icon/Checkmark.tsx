c3cf675d2d6707fd461c1b13c6f18a2411a34405
ShawTim <shawtim.liu@gmail.com>
2022-04-04 16:54:43 +0800

    SwitcherButton for network/wallet switcher (#1355)
    * SwitcherButton for network/wallet switcher
    
    * export SwitcherButton from shared
    
    * snapshot test for SwitcherButton
    
    * remove useMemo if not necessary
    
    remove useMemo

--- packages/tempus-client_v3/src/components/shared/Icon/Checkmark.tsx
+++ packages/tempus-client_v3/src/components/shared/Icon/Checkmark.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
-import { IconProps, ICON_SIZE_DEFAULT } from './Icon';
+import { IconProps, ICON_COLOR_DEFAULT, ICON_SIZE_DEFAULT } from './Icon';
 import withIcon from './withIcon';
 
-const Checkmark: FC<IconProps> = ({ size = ICON_SIZE_DEFAULT }) => (
+const Checkmark: FC<IconProps> = ({ size = ICON_SIZE_DEFAULT, color = ICON_COLOR_DEFAULT }) => (
   <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 8.1003L6 14L16 4L14 2L6 10L2 6L0 8.1003Z" fill="white" />
+    <path d="M0 8.1003L6 14L16 4L14 2L6 10L2 6L0 8.1003Z" fill={color} />
   </svg>
 );
 
