5b73f7ec667d6fda426b201eb15eb27cbdf7be87
Vukašin Manojlović <iamvukasin@gmail.com>
2022-04-05 09:03:22 +0200

    Move `TermTab` to separate file

---
+++ packages/tempus-client_v3/src/components/shared/TermTabs/TermTab.tsx
@@ -0,0 +1,33 @@
+import { FC, useCallback } from 'react';
+import ButtonWrapper from '../ButtonWrapper';
+import FormattedDate from '../FormattedDate';
+import Spacer from '../Spacer';
+import Typography from '../Typography';
+
+interface TermTabProps {
+  date: Date;
+  selected: boolean;
+  onClick: (selected: Date) => void;
+}
+
+const TermTab: FC<TermTabProps> = props => {
+  const { date, selected, onClick } = props;
+  const handleClick = useCallback(() => onClick(date), [date, onClick]);
+
+  return (
+    <ButtonWrapper
+      className={`tc__term-tabs__term ${selected ? 'tc__term-tabs__term-selected' : ''}`}
+      onClick={handleClick}
+    >
+      <Typography variant="body-secondary" weight="medium">
+        Term
+      </Typography>
+      <Spacer size={4} />
+      <Typography variant="body-primary" type="mono">
+        <FormattedDate date={date} size="medium" separatorContrast="high" />
+      </Typography>
+    </ButtonWrapper>
+  );
+};
+
+export default TermTab;
