f7fb604ab8dde998be78e7eaa5902bc6a3ec9582
Lazar Mitic <Lazar__93@hotmail.com>
2022-04-06 11:59:25 +0200

    Fix Checkbox Storybook

--- packages/tempus-client_v3/src/components/shared/Checkbox/Checkbox.tsx
+++ packages/tempus-client_v3/src/components/shared/Checkbox/Checkbox.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FC, useMemo } from 'react';
-import { Typography } from '..';
+import Typography from '../Typography';
 import './checkbox.scss';
 
 export interface CheckboxProps {
