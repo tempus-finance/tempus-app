c0e0a33ab7d1c8c835028f6577b7883c2edf785e
Lazar Mitic <Lazar__93@hotmail.com>
2022-04-25 10:44:51 +0200

    Add blocknative support

--- packages/tempus-client_v3/src/components/Navbar/Navbar.tsx
+++ packages/tempus-client_v3/src/components/Navbar/Navbar.tsx
@@ -1,32 +1,67 @@
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
+import { init, useConnectWallet } from '@web3-onboard/react';
+import injectedModule from '@web3-onboard/injected-wallets';
 import { ButtonWrapper, colors, Icon, Logo, Typography, WalletButton } from '../shared';
 
 import './Navbar.scss';
 
-const Navbar: FC = () => (
-  <div className="tc__navbar">
-    <Logo type="protocol-Tempus" />
-    <div className="tc__navbar-actions">
-      <ButtonWrapper>
-        <div className="tc__navbar-actions-dropdown">
-          <Typography variant="body-primary" color="text-primary-inverted">
-            Settings
-          </Typography>
-          <div className="tc__navbar-actions-dropdown-icon">
-            <Icon variant="down-chevron" size={12} color={colors.textPrimaryInverted} />
+const injected = injectedModule();
+
+init({
+  wallets: [injected],
+  chains: [
+    {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum Mainnet',
+      rpcUrl: 'https://eth-mainnet.alchemyapi.io/v2/CikBd5PRY8HTC1rBQw1dBq5HlOfXwe0O',
+    },
+  ],
+  appMetadata: {
+    name: 'Tempus App',
+    icon: '<svg><svg/>',
+    description: 'Official Tempus App',
+    recommendedInjectedWallets: [{ name: 'MetaMask', url: 'https://metamask.io' }],
+  },
+  accountCenter: {
+    desktop: {
+      enabled: false,
+    },
+  },
+});
+
+const Navbar: FC = () => {
+  const [{ wallet }, connect] = useConnectWallet();
+
+  const onConnectWallet = useCallback(() => {
+    connect({});
+  }, [connect]);
+
+  return (
+    <div className="tc__navbar">
+      <Logo type="protocol-Tempus" />
+      <div className="tc__navbar-actions">
+        <ButtonWrapper>
+          <div className="tc__navbar-actions-dropdown">
+            <Typography variant="body-primary" color="text-primary-inverted">
+              Settings
+            </Typography>
+            <div className="tc__navbar-actions-dropdown-icon">
+              <Icon variant="down-chevron" size={12} color={colors.textPrimaryInverted} />
+            </div>
           </div>
-        </div>
-      </ButtonWrapper>
-      {/* TODO - Integrate wallet button logic */}
-      <WalletButton
-        address=""
-        balance=""
-        chain="ethereum"
-        onConnect={() => {}}
-        onNetworkClick={() => {}}
-        onWalletClick={() => {}}
-      />
+        </ButtonWrapper>
+        {/* TODO - Integrate wallet button logic */}
+        <WalletButton
+          address={wallet?.accounts[0].address || ''}
+          balance=""
+          chain="ethereum"
+          onConnect={onConnectWallet}
+          onNetworkClick={() => {}}
+          onWalletClick={() => {}}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Navbar;
