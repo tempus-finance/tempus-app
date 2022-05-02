98e1ea5efb1346af3e3aead910aa2c40a655dafd
irinelT <irinel@tempus.finance>
2022-05-02 18:01:09 +0200

    New localhost address (attempt 3).

--- integration-testing/utility/constants.ts
+++ integration-testing/utility/constants.ts
@@ -1,7 +1,7 @@
 export const USER_DATA_DIR: string = 'integration-testing/utility/user_data';
 export const METAMASK_PATH: string = 'integration-testing/mm';
 export const TEMPUS_URL: string
-    = process.env.CI ? 'http://127.0.0.1:3000' : 'https://tempus-app-stage.web.app/';
+    = process.env.CI ? 'http://127.0.0.1' : 'https://tempus-app-stage.web.app/';
 export const LOAD_TIMEOUT: number = 600;
 export const LOAD_SHORT_TIMEOUT: number = 200;
 export const LOAD_LONG_TIMEOUT: number = 1800;
