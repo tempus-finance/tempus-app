93da347c537ea9a8f56e5d2d6b4bd531e0dd65ed
ShawTim <shawtim.liu@gmail.com>
2022-05-06 22:28:32 +0800

    unit test for useLocale hook

---
+++ packages/tempus-client_v3/src/hooks/useLocale.spec.ts
@@ -0,0 +1,52 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-dom/test-utils';
+import { SUPPORTED_LOCALES, SUPPORTED_LOCALE_NAMES, useLocale } from './useLocale';
+
+describe('useLocale', () => {
+  it('by default it return en', () => {
+    const { result } = renderHook(() => useLocale());
+    const [locale] = result.current;
+
+    expect(locale).toEqual('en');
+  });
+
+  it('update locale to es', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useLocale());
+    const [locale, setLocale] = result.current;
+
+    expect(locale).toEqual('en');
+
+    await act(async () => {
+      setLocale('es');
+      await waitForNextUpdate();
+    });
+
+    expect(result.current[0]).toEqual('es');
+  });
+
+  it('update locale to it', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useLocale());
+    const [locale, setLocale] = result.current;
+
+    expect(locale).toEqual('en');
+
+    await act(async () => {
+      setLocale('it');
+      await waitForNextUpdate();
+    });
+
+    expect(result.current[0]).toEqual('it');
+  });
+
+  it('it export SUPPORTED_LOCALES', () => {
+    expect(SUPPORTED_LOCALES).toEqual(['en', 'es', 'it']);
+  });
+
+  it('it export SUPPORTED_LOCALE_NAMES', () => {
+    expect(SUPPORTED_LOCALE_NAMES).toEqual({
+      en: 'English',
+      es: 'Español',
+      it: 'Italiano',
+    });
+  });
+});
--- packages/tempus-client_v3/src/hooks/useLocale.ts
+++ packages/tempus-client_v3/src/hooks/useLocale.ts
@@ -2,22 +2,21 @@ import { state, useStateObservable } from '@react-rxjs/core';
 import { createSignal } from '@react-rxjs/utils';
 import { useCallback } from 'react';
 
-export const SUPPORTED_LOCALES = ['en', 'es', 'it'] as const;
-export type SupportedLocale = typeof SUPPORTED_LOCALES[number];
-export const SUPPORTED_LOCALE_NAMES = SUPPORTED_LOCALES.reduce(
-  (map, code) => ({
-    ...map,
-    [code]: new Intl.DisplayNames(code, { type: 'language' }).of(code) || code,
-  }),
-  {},
-) as Record<SupportedLocale, string>;
-
 const [locale$, setLocale] = createSignal<SupportedLocale>();
 const state$ = state(locale$, 'en');
 
+export const SUPPORTED_LOCALES = ['en', 'es', 'it'] as const;
+export type SupportedLocale = typeof SUPPORTED_LOCALES[number];
+export const SUPPORTED_LOCALE_NAMES: Record<SupportedLocale, string> = {
+  // new Intl.DisplayNames(code, { type: 'language' }).of(code)
+  en: 'English',
+  es: 'Español',
+  it: 'Italiano',
+};
+
 export function useLocale(): [SupportedLocale, (value: SupportedLocale) => void] {
   const hook = useStateObservable(state$);
-  const setLocaleCode = useCallback((value: SupportedLocale = 'en') => setLocale(value), []);
+  const setLocaleCode = useCallback((value: SupportedLocale) => setLocale(value), []);
 
   return [hook, setLocaleCode];
 }
