c3898a8f39aaa862f4f1b0338421b79f394c8c0b
Vukašin Manojlović <iamvukasin@gmail.com>
2022-05-13 13:03:49 +0200

    Fix pseudo translation script to support macOS

--- packages/tempus-client_v3/scripts/pseudo-translation.sh
+++ packages/tempus-client_v3/scripts/pseudo-translation.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sed -i -E 's/"(.+)": ""/"\1": "~~~~~====={{{{{(((((\1)))))}}}}}=====~~~~~"/g' src/i18n/translations/zz.json
+TRANSLATION_FILE=src/i18n/translations/zz.json
+sed -i.bak -E 's/"(.+)": ""/"\1": "~~~~~====={{{{{(((((\1)))))}}}}}=====~~~~~"/g' $TRANSLATION_FILE && rm "${TRANSLATION_FILE}.bak"
