475030fe99d902edfdedbabec08e2404ac4d935d
Vukašin Manojlović <iamvukasin@gmail.com>
2022-05-18 12:08:54 +0200

    Separate modal's preview and action content

--- packages/tempus-client_v3/src/components/CurrencyInputModal/CurrencyInputModal.tsx
+++ packages/tempus-client_v3/src/components/CurrencyInputModal/CurrencyInputModal.tsx
@@ -1,21 +1,11 @@
-import { FC, ReactNode, useCallback, useMemo, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { FC, ReactNode, useCallback, useState } from 'react';
 import { ChainConfig, Decimal, Ticker } from 'tempus-core-services';
-import FeeTooltip from '../FeeTooltip';
-import {
-  ActionButton,
-  ActionButtonLabels,
-  ActionButtonState,
-  CurrencyInput,
-  Icon,
-  Link,
-  Modal,
-  ProgressBar,
-  Typography,
-} from '../shared';
+import { ActionButtonLabels, ActionButtonState, Modal, Typography } from '../shared';
 import { ModalProps } from '../shared/Modal/Modal';
 import TermTabs, { MaturityTerm } from '../shared/TermTabs';
 import './CurrencyInputModal.scss';
+import ModalActionContent from './ModalActionContent';
+import ModalPreviewContent from './ModalPreviewContent';
 
 type CurrencyInputModalContent = 'preview' | 'action';
 
@@ -69,33 +59,12 @@ const CurrencyInputModal: FC<CurrencyInputModalProps> = props => {
   } = props;
 
   const [content, setContent] = useState<CurrencyInputModalContent>(preview ? 'preview' : 'action');
-  const [amount, setAmount] = useState('');
-  const [transactionProgress, setTransactionProgress] = useState<number | null>(null);
-  const [transactionHash, setTransactionHash] = useState<string | null>(null);
-  const { t } = useTranslation();
-
-  const amountDecimal = useMemo(() => new Decimal(amount || 0), [amount]);
-
-  const disabledInput = actionButtonState !== 'default';
-  const insufficientBalance = amountDecimal.gt(balance);
 
   const handlePreviewButtonClick = useCallback(() => setContent('action'), []);
 
   const handleBack = useCallback(() => setContent('preview'), []);
 
-  const handleAmountChange = useCallback(
-    (value: string) => {
-      setAmount(value);
-      onAmountChange?.(Decimal.parse(value, 0));
-    },
-    [onAmountChange],
-  );
-
-  const handleActionButtonClick = useCallback(() => {
-    // TODO: this is a mockup, replace with real implementation
-    setTransactionProgress(20);
-    setTransactionHash(onTransactionStart(amountDecimal));
-  }, [amountDecimal, onTransactionStart]);
+  const disabledInput = actionButtonState !== 'default';
 
   return (
     <Modal
@@ -116,63 +85,27 @@ const CurrencyInputModal: FC<CurrencyInputModalProps> = props => {
         <TermTabs terms={maturityTerms} disabled={disabledInput} onChange={onMaturityChange} />
       )}
       {content === 'preview' && (
-        <>
+        <ModalPreviewContent
+          actionButtonLabels={actionButtonLabels.preview ?? actionButtonLabels.action}
+          onActionButtonClick={handlePreviewButtonClick}
+        >
           {preview}
-          <div className="tc__currency-input-modal__action-container">
-            <ActionButton
-              labels={actionButtonLabels.preview ?? actionButtonLabels.action}
-              onClick={handlePreviewButtonClick}
-              variant="primary"
-              size="large"
-              fullWidth
-              state="default"
-            />
-          </div>
-        </>
+        </ModalPreviewContent>
       )}
       {content === 'action' && (
-        <>
-          <CurrencyInput
-            precision={inputPrecision}
-            maxAmount={balance}
-            usdRates={usdRates}
-            disabled={disabledInput}
-            error={insufficientBalance ? 'Insufficient balance' : undefined}
-            onAmountUpdate={handleAmountChange}
-            onCurrencyUpdate={onCurrencyUpdate}
-          />
-          {infoRows && <div className="tc__currency-input-modal__info">{infoRows}</div>}
-          <div className="tc__currency-input-modal__action-container">
-            <FeeTooltip fees={{ swap: 0.002 }}>
-              <div className="tc__currency-input-modal__transaction-info">
-                <Typography variant="body-primary">{t('CurrencyInputModal.feesAndTransactionInfo')}</Typography>
-                <Icon variant="info-bordered" size="small" />
-              </div>
-            </FeeTooltip>
-            <ActionButton
-              labels={actionButtonLabels.action}
-              onClick={handleActionButtonClick}
-              variant="primary"
-              size="large"
-              fullWidth
-              state={amountDecimal.lte(0) || insufficientBalance ? 'disabled' : actionButtonState}
-            />
-            {transactionProgress !== null && actionButtonState !== 'default' && (
-              <ProgressBar value={actionButtonState === 'success' ? 100 : transactionProgress} />
-            )}
-            {chainConfig && transactionHash && actionButtonState !== 'default' && (
-              <Link
-                href={`${chainConfig.blockExplorerUrl}tx/${transactionHash}`}
-                className="tc__currency-input-modal__transaction-link"
-              >
-                <Typography variant="body-secondary">
-                  {t('CurrencyInputModal.linkBlockExplorer', { name: chainConfig.blockExplorerName })}
-                </Typography>
-                <Icon variant="external" size="small" />
-              </Link>
-            )}
-          </div>
-        </>
+        <ModalActionContent
+          balance={balance}
+          inputPrecision={inputPrecision}
+          disabledInput={disabledInput}
+          usdRates={usdRates}
+          chainConfig={chainConfig}
+          infoRows={infoRows}
+          actionButtonLabels={actionButtonLabels.action}
+          actionButtonState={actionButtonState}
+          onAmountChange={onAmountChange}
+          onTransactionStart={onTransactionStart}
+          onCurrencyUpdate={onCurrencyUpdate}
+        />
       )}
     </Modal>
   );
