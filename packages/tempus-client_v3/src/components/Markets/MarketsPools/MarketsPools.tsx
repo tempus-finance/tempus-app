a63718547db44882722164e751153c40c3a98bb4
Lazar Mitic <Lazar__93@hotmail.com>
2022-05-18 16:57:01 +0200

    Add pool card color based on backing token (#1632)

--- packages/tempus-client_v3/src/components/Markets/MarketsPools/MarketsPools.tsx
+++ packages/tempus-client_v3/src/components/Markets/MarketsPools/MarketsPools.tsx
@@ -1,4 +1,4 @@
-import { Decimal } from 'tempus-core-services';
+import { Decimal, tokenColorMap } from 'tempus-core-services';
 import { usePoolList } from '../../../hooks';
 import { PoolCard } from '../../shared';
 import './MarketsPools.scss';
@@ -8,18 +8,26 @@ const MarketsPools = (): JSX.Element => {
 
   return (
     <div className="tc__marketsPools">
-      {tempusPools.map(tempusPool => (
-        <PoolCard
-          key={`${tempusPool.chain}-${tempusPool.address}`}
-          aprValues={[new Decimal(0.1)]}
-          color="#ff5500"
-          poolCardStatus="Fixed"
-          poolCardVariant="markets"
-          ticker={tempusPool.backingToken}
-          protocol="aave"
-          terms={[new Date(4, 5, 2023)]}
-        />
-      ))}
+      {tempusPools.map(tempusPool => {
+        const cardColor = tokenColorMap.get(tempusPool.backingToken);
+
+        if (!cardColor) {
+          console.warn(`Missing ${tempusPool.backingToken} token color in tokenColorMap!`);
+        }
+
+        return (
+          <PoolCard
+            key={`${tempusPool.chain}-${tempusPool.address}`}
+            aprValues={[new Decimal(0.1)]}
+            color={cardColor || '#ffffff'}
+            poolCardStatus="Fixed"
+            poolCardVariant="markets"
+            ticker={tempusPool.backingToken}
+            protocol="aave"
+            terms={[new Date(4, 5, 2023)]}
+          />
+        );
+      })}
     </div>
   );
 };
