a2815c04720b73cd67e593284263f067d767a5fe
Lazar <Lazar__93@hotmail.com>
2022-05-18 13:46:45 +0200

    Show list of pools on markets page

--- packages/tempus-client_v3/src/components/Markets/Markets.tsx
+++ packages/tempus-client_v3/src/components/Markets/Markets.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import MarketsSubheader from './MarketsSubheader';
+import MarketsPools from './MarketsPools';
 import './Markets.scss';
 
 const Markets: FC = () => (
   <div className="tc__app__markets">
     <MarketsSubheader />
+    <MarketsPools />
   </div>
 );
 
---
+++ packages/tempus-client_v3/src/components/Markets/MarketsPools/MarketsPools.tsx
@@ -0,0 +1,25 @@
+import { Decimal } from 'tempus-core-services';
+import { usePoolList } from '../../../hooks';
+import { PoolCard } from '../../shared';
+import './MarketsPools.scss';
+
+const MarketsPools = (): JSX.Element => {
+  const tempusPools = usePoolList();
+
+  return (
+    <div className="tc__marketsPools">
+      {tempusPools.map(tempusPool => (
+        <PoolCard
+          aprValues={[new Decimal(0.1)]}
+          color="#ff5500"
+          poolCardStatus="Fixed"
+          poolCardVariant="markets"
+          ticker={tempusPool.backingToken}
+          protocol="aave"
+          terms={[new Date(4, 5, 2023)]}
+        />
+      ))}
+    </div>
+  );
+};
+export default MarketsPools;
