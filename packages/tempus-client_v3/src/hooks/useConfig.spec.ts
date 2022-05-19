792e83b65d47fdf8881d1f42b3385401f0075b88
Lazar Mitic <Lazar__93@hotmail.com>
2022-05-19 14:11:12 +0200

    Replace hardcoded protocol value inside markets cards (#1647)

--- packages/tempus-client_v3/src/hooks/useConfig.spec.ts
+++ packages/tempus-client_v3/src/hooks/useConfig.spec.ts
@@ -22,24 +22,28 @@ describe.only('useConfig', () => {
         backingToken: 'ETH',
         backingTokenAddress: '0x0000000000000000000000000000000000000000',
         chain: 'ethereum',
+        protocol: 'lido',
       },
       {
         address: '2',
         backingToken: 'USDC',
         backingTokenAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
         chain: 'ethereum',
+        protocol: 'yearn',
       },
       {
         address: '3',
         backingToken: 'USDC',
         backingTokenAddress: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
         chain: 'fantom',
+        protocol: 'yearn',
       },
       {
         address: '4',
         backingToken: 'WETH',
         backingTokenAddress: '0x74b23882a30290451A17c44f4F05243b6b58C76d',
         chain: 'fantom',
+        protocol: 'yearn',
       },
     ]);
   });
