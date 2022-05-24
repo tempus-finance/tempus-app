8691b62accd2cbbef5d3ba33320111fe79779482
ShawTim <shawtim.liu@gmail.com>
2022-05-24 16:23:01 +0800

    break down TVL data for `usePoolViewOptions` (#1672)
    * implementation only
    
    * remove console.log
    
    * undo testing code
    
    * fixed unit tests
    
    * minor enhencement on setupTests
    
    * fix build error

--- packages/tempus-client_v3/src/hooks/useTvlData.ts
+++ packages/tempus-client_v3/src/hooks/useTvlData.ts
@@ -1,34 +1,69 @@
 import { bind } from '@react-rxjs/core';
-import { catchError, from, interval, map, mergeMap, Observable, of, startWith, zip } from 'rxjs';
-import { getServices, Decimal, StatisticsService, TempusPool, Chain } from 'tempus-core-services';
-import { getConfigManager } from '../config/getConfigManager';
+import {
+  catchError,
+  combineLatest,
+  debounce,
+  interval,
+  map,
+  merge,
+  mergeMap,
+  Observable,
+  of,
+  scan,
+  startWith,
+} from 'rxjs';
+import { getServices, Decimal, ZERO, StatisticsService, TempusPool, Chain } from 'tempus-core-services';
+import { poolList$ } from './useConfig';
 
-const ZERO = new Decimal(0);
+interface PoolTvlMap {
+  [chainPoolAddress: string]: Decimal;
+}
+
 const TVL_POLLING_INTERVAL_IN_MS = 10000;
+const DEBOUNCE_IN_MS = 500;
 
-const tvlData$: Observable<Decimal> = interval(TVL_POLLING_INTERVAL_IN_MS).pipe(
-  startWith(0),
-  map(() => getConfigManager().getPoolList()),
-  mergeMap<TempusPool[], Observable<Decimal[]>>((tempusPools: TempusPool[]) =>
-    zip<Decimal[]>(
-      tempusPools.map(({ chain, address, backingToken }: TempusPool) =>
-        from<Observable<Decimal>>(
-          (getServices(chain as Chain)?.StatisticsService as StatisticsService).totalValueLockedUSD(
-            chain as Chain,
-            address,
-            backingToken,
-          ),
+const polling$: Observable<number> = interval(TVL_POLLING_INTERVAL_IN_MS).pipe(startWith(0));
+
+export const poolTvls$: Observable<PoolTvlMap> = combineLatest([poolList$, polling$]).pipe(
+  mergeMap<[TempusPool[], number], Observable<PoolTvlMap>>(([tempusPools]) => {
+    const poolTvlMaps = tempusPools.map(({ chain, address, backingToken }: TempusPool) => {
+      const poolTvl$ = (getServices(chain as Chain)?.StatisticsService as StatisticsService).totalValueLockedUSD(
+        chain as Chain,
+        address,
+        backingToken,
+      );
+      return poolTvl$.pipe(
+        map(
+          tvl =>
+            ({
+              [`${chain}-${address}`]: tvl,
+            } as PoolTvlMap),
         ),
-      ),
-    ),
+      );
+    });
+
+    return merge(...poolTvlMaps);
+  }),
+  scan(
+    (allTvls, poolTvl) => ({
+      ...allTvls,
+      ...poolTvl,
+    }),
+    {} as PoolTvlMap,
   ),
-  map<Decimal[], Decimal>((poolLockedValues: Decimal[]) =>
-    poolLockedValues.reduce((memo: Decimal, current: Decimal) => memo.add(current)),
-  ),
+  debounce<PoolTvlMap>(() => interval(DEBOUNCE_IN_MS)),
   catchError(error => {
     console.error('useTvlData - getTempusPoolTVL', error);
-    return of(ZERO);
+    return of({});
   }),
 );
 
-export const [useTvlData] = bind(tvlData$, ZERO);
+const totalTvl$: Observable<Decimal> = poolTvls$.pipe(
+  map(poolTvls => {
+    const allTvls = Object.values(poolTvls);
+    return allTvls.reduce((sum, tvl) => sum.add(tvl), ZERO);
+  }),
+);
+
+export const [useTvlData] = bind(poolTvls$, {});
+export const [useTotalTvl] = bind(totalTvl$, ZERO);
