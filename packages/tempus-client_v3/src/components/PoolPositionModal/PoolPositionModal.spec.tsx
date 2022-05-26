f5dbfd7b29ecdfbe2080c2b42d6e544ea6311dd9
Lazar <Lazar__93@hotmail.com>
2022-05-26 08:15:09 +0200

    Add snapshot test

---
+++ packages/tempus-client_v3/src/components/PoolPositionModal/PoolPositionModal.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import { Decimal } from 'tempus-core-services';
+import PoolPositionModal from './PoolPositionModal';
+
+const subject = () =>
+  render(
+    <PoolPositionModal
+      apr={0.1}
+      balance={new Decimal(20)}
+      chartData={[
+        { x: new Date(2022, 3, 4), y: 10 },
+        { x: new Date(2022, 4, 1), y: 20 },
+      ]}
+      onWithdraw={() => {}}
+      profitLoss={new Decimal(2)}
+      projectedTotalYield={new Decimal(3)}
+      term={new Date(2022, 4, 2)}
+      tokenDecimals={4}
+      tokenExchangeRate={new Decimal(100)}
+      totalYieldEarned={new Decimal(2)}
+      tokenTicker="ETH"
+    />,
+  );
+
+describe('PoolPositionModal', () => {
+  it('renders a pool position modal', () => {
+    const { container } = subject();
+
+    expect(container).not.toBeNull();
+    expect(container).toMatchSnapshot();
+  });
+});
