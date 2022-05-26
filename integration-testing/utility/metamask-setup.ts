a09ea44f5e2e30accf0f8cb3b1f05b714579260a
irinelT <irinel@tempus.finance>
2022-05-26 14:11:37 +0200

    revert and fixes

--- integration-testing/utility/metamask-setup.ts
+++ integration-testing/utility/metamask-setup.ts
@@ -6,8 +6,10 @@ import { tempusMetamaskConnect } from '../modules/tempushome';
 (async () => {
   const browser: BrowserContext = await chromiumPersistant();
   await metamaskRegister(browser);
+  console.log('Metamask account registered');
   await metamaskLogin(browser);
   await tempusMetamaskConnect(browser);
+  console.log('Successful connection metamask-tempus');
   // TODO: add all metamask accounts and switch to 0, currently not needed
   await browser.close();
 })();
