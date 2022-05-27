6149cacec766bf44d383766edbfbbd2246dadeb8
Vukašin Manojlović <iamvukasin@gmail.com>
2022-05-27 11:35:31 +0200

    Convert `useFixedApr` to pool-APR map

--- packages/tempus-client_v3/src/hooks/useFixedApr.spec.ts
+++ packages/tempus-client_v3/src/hooks/useFixedApr.spec.ts
@@ -1,6 +1,6 @@
-import { act, renderHook } from '@testing-library/react-hooks';
-import { delay, of } from 'rxjs';
-import { Decimal, getServices, TempusPool } from 'tempus-core-services';
+import { renderHook } from '@testing-library/react-hooks';
+import { of } from 'rxjs';
+import { Decimal, getServices } from 'tempus-core-services';
 import { useFixedApr } from './useFixedApr';
 
 jest.mock('tempus-core-services', () => ({
@@ -16,63 +16,16 @@ describe('useFixedApr', () => {
     jest.resetAllMocks();
   });
 
-  it('returns `0` as initial value', async () => {
-    const [useApr] = useFixedApr({ address: 'abc', spotPrice: '3' } as TempusPool);
-    const { result } = renderHook(() => useApr());
-
-    expect(mockEstimatedDepositAndFix).not.toHaveBeenCalled();
-    expect(mockEstimatedMintedShares).not.toHaveBeenCalled();
-    expect(result.current).toEqual(new Decimal('0'));
-  });
-
-  it('returns a value based on spot price if token amount is not set', async () => {
+  it('returns `{}` as initial value', async () => {
     (getServices as unknown as jest.Mock).mockImplementation(() => ({
       StatisticsService: {
-        estimatedDepositAndFix: mockEstimatedDepositAndFix.mockImplementation(() =>
-          of<Decimal>(new Decimal('1500')).pipe(delay(1000)),
-        ),
-        estimatedMintedShares: mockEstimatedMintedShares.mockImplementation(() =>
-          of<Decimal>(new Decimal('200')).pipe(delay(1000)),
-        ),
+        estimatedDepositAndFix: mockEstimatedDepositAndFix.mockImplementation(() => of<Decimal>(new Decimal('1500'))),
+        estimatedMintedShares: mockEstimatedMintedShares.mockImplementation(() => of<Decimal>(new Decimal('200'))),
       },
     }));
 
-    const [useApr] = useFixedApr({ address: 'abc', spotPrice: '3' } as TempusPool);
-    const { waitForNextUpdate, result } = renderHook(() => useApr());
+    const { result } = renderHook(() => useFixedApr());
 
-    expect(result.current).toEqual(new Decimal('0'));
-
-    await waitForNextUpdate({ timeout: 5000 });
-
-    expect(mockEstimatedDepositAndFix).toHaveBeenCalledTimes(1);
-    expect(mockEstimatedMintedShares).toHaveBeenCalledTimes(1);
-    expect(result.current).toEqual(new Decimal('6.5'));
-  });
-
-  it('returns a value when pool and token amount are set', async () => {
-    (getServices as unknown as jest.Mock).mockImplementation(() => ({
-      StatisticsService: {
-        estimatedDepositAndFix: mockEstimatedDepositAndFix.mockImplementation(() =>
-          of<Decimal>(new Decimal('1000')).pipe(delay(1000)),
-        ),
-        estimatedMintedShares: mockEstimatedMintedShares.mockImplementation(() =>
-          of<Decimal>(new Decimal('100')).pipe(delay(1000)),
-        ),
-      },
-    }));
-
-    const [useApr, setTokenAmount] = useFixedApr({ address: 'xyz', spotPrice: '5' } as TempusPool);
-    const { waitForNextUpdate, result } = renderHook(() => useApr());
-
-    expect(result.current).toEqual(new Decimal('0'));
-
-    act(() => {
-      setTokenAmount(new Decimal('120'));
-    });
-    await waitForNextUpdate({ timeout: 5000 });
-
-    expect(mockEstimatedDepositAndFix).toHaveBeenCalledTimes(1);
-    expect(mockEstimatedMintedShares).toHaveBeenCalledTimes(1);
-    expect(result.current).toEqual(new Decimal('9'));
+    expect(result.current).toEqual({});
   });
 });
--- packages/tempus-client_v3/src/hooks/useFixedApr.ts
+++ packages/tempus-client_v3/src/hooks/useFixedApr.ts
@@ -1,112 +1,106 @@
 import { bind } from '@react-rxjs/core';
-import { createSignal } from '@react-rxjs/utils';
 import {
   catchError,
   combineLatest,
-  combineLatestWith,
-  filter,
+  debounce,
   from,
   interval,
   map,
+  merge,
+  mergeMap,
   Observable,
   of,
+  scan,
   startWith,
   switchMap,
-  take,
 } from 'rxjs';
-import { getServices, Decimal, StatisticsService, TempusPool, Chain } from 'tempus-core-services';
+import { getServices, Decimal, StatisticsService, TempusPool, Chain, ZERO, ONE } from 'tempus-core-services';
+import { poolList$ } from './useConfig';
 
-// TODO move to constants
-const ZERO = new Decimal(0);
-const ONE = new Decimal('1');
+interface PoolFixedAprMap {
+  [chainPoolAddress: string]: Decimal;
+}
 
 // TODO all polling interval constants in a file?
-const APR_POLLING_INTERVAL_IN_MS = 5000;
+const APR_POLLING_INTERVAL_IN_MS = 120000;
+const DEBOUNCE_IN_MS = 500;
 
-const [tempusPool$, setTempusPool] = createSignal<TempusPool>();
-const [tokenAmount$, setTokenAmount] = createSignal<Decimal>();
+const intervalBeat$: Observable<number> = interval(APR_POLLING_INTERVAL_IN_MS).pipe(startWith(0));
 
-const aprData$ = (): Observable<Decimal> => {
-  const intervalBeat$: Observable<number> = interval(APR_POLLING_INTERVAL_IN_MS).pipe(startWith(0));
-
-  const estimatedDepositAndFix$ = (
-    statisticsService: StatisticsService,
-    tempusAMMAddress: string,
-    tokenAmount: Decimal,
-    isBackingToken: boolean,
-  ): Observable<Decimal> =>
-    from(
-      (statisticsService as StatisticsService).estimatedDepositAndFix(
-        tempusAMMAddress,
-        tokenAmount.toBigNumber(),
-        isBackingToken,
-      ),
-    ).pipe(switchMap(value => of(new Decimal(value))));
-
-  const estimatedMintedShares$ = (
-    statisticsService: StatisticsService,
-    tempusPoolAddress: string,
-    tokenAmount: Decimal,
-    isBackingToken: boolean,
-    callOverrideData?: { blockTag: number },
-  ): Observable<Decimal> =>
-    from(
-      (statisticsService as StatisticsService).estimatedMintedShares(
-        tempusPoolAddress,
-        tokenAmount.toBigNumber(),
-        isBackingToken,
-        callOverrideData,
-      ),
-    ).pipe(switchMap(value => of(new Decimal(value))));
-
-  return combineLatest([tempusPool$, intervalBeat$]).pipe(
-    map(([tempusPool]) => tempusPool),
-    combineLatestWith(tokenAmount$.pipe(startWith(ZERO))),
-    map(([tempusPool, tokenAmount]) => ({
-      tempusPool,
-      tokenAmount: tokenAmount.gt(ZERO) ? tokenAmount : new Decimal(tempusPool.spotPrice),
-      statisticsService: getServices(tempusPool.chain as Chain)?.StatisticsService as StatisticsService,
-    })),
-    filter(
-      (result: { tempusPool: TempusPool; tokenAmount: Decimal; statisticsService: StatisticsService }) =>
-        !!result.statisticsService,
+const estimatedDepositAndFix$ = (
+  statisticsService: StatisticsService,
+  tempusAMMAddress: string,
+  tokenAmount: Decimal,
+  isBackingToken: boolean,
+): Observable<Decimal> =>
+  from(
+    (statisticsService as StatisticsService).estimatedDepositAndFix(
+      tempusAMMAddress,
+      tokenAmount.toBigNumber(),
+      isBackingToken,
     ),
-    take(1),
-    switchMap(
-      ({
-        tempusPool,
-        tokenAmount,
-        statisticsService,
-      }: {
-        tempusPool: TempusPool;
-        tokenAmount: Decimal;
-        statisticsService: StatisticsService;
-      }) =>
-        combineLatest([
-          intervalBeat$,
-          estimatedDepositAndFix$(statisticsService, tempusPool.ammAddress, tokenAmount, true),
-          estimatedMintedShares$(statisticsService, tempusPool.address, tokenAmount, true),
-        ]),
+  ).pipe(switchMap(value => of(new Decimal(value))));
+
+const estimatedMintedShares$ = (
+  statisticsService: StatisticsService,
+  tempusPoolAddress: string,
+  tokenAmount: Decimal,
+  isBackingToken: boolean,
+  callOverrideData?: { blockTag: number },
+): Observable<Decimal> =>
+  from(
+    (statisticsService as StatisticsService).estimatedMintedShares(
+      tempusPoolAddress,
+      tokenAmount.toBigNumber(),
+      isBackingToken,
+      callOverrideData,
     ),
-    map(([, principals, estimatedMintedShares]) => {
-      const ratio = principals.gt(ZERO) ? principals.div(estimatedMintedShares) : ZERO;
-      const pureInterest = ratio.sub(ONE);
+  ).pipe(switchMap(value => of(new Decimal(value))));
 
-      // TODO needs to be calculated properly
-      const scaleFactor = ONE;
-      return pureInterest.mul(scaleFactor);
-    }),
-    catchError(error => {
-      console.error('useFixedApr - aprData', error);
-      return of(ZERO);
-    }),
-  );
-};
+const poolAprs$: Observable<PoolFixedAprMap> = combineLatest([poolList$, intervalBeat$]).pipe(
+  mergeMap<[TempusPool[], number], Observable<PoolFixedAprMap>>(([tempusPools]) => {
+    const poolAprMaps = tempusPools.map(({ ammAddress, address, chain, spotPrice }) => {
+      const tokenAmount = new Decimal(spotPrice);
+      const statisticsService = getServices(chain as Chain)?.StatisticsService as StatisticsService;
 
-export const useFixedApr = (tp: TempusPool): [() => Decimal, (tokenAmount: Decimal) => void] => {
-  const [useApr] = bind(aprData$(), ZERO);
-  setTimeout(() => {
-    setTempusPool(tp);
-  }, 0);
-  return [useApr, setTokenAmount];
-};
+      const principals$ = estimatedDepositAndFix$(statisticsService, ammAddress, tokenAmount, true);
+      const mintShares$ = estimatedMintedShares$(statisticsService, address, tokenAmount, true);
+
+      const fixedApr$ = combineLatest([principals$, mintShares$]).pipe(
+        map(([principals, estimatedMintedShares]) => {
+          const ratio = principals.gt(ZERO) ? principals.div(estimatedMintedShares) : ZERO;
+          const pureInterest = ratio.sub(ONE);
+
+          // TODO needs to be calculated properly
+          const scaleFactor = ONE;
+          return pureInterest.mul(scaleFactor);
+        }),
+      );
+
+      return fixedApr$.pipe(
+        map(
+          apr =>
+            ({
+              [`${chain}-${address}`]: apr,
+            } as PoolFixedAprMap),
+        ),
+      );
+    });
+
+    return merge(...poolAprMaps);
+  }),
+  scan(
+    (allAprs, poolApr) => ({
+      ...allAprs,
+      ...poolApr,
+    }),
+    {} as PoolFixedAprMap,
+  ),
+  debounce(() => interval(DEBOUNCE_IN_MS)),
+  catchError(error => {
+    console.error('useFixedApr - Failed to fetch fixed APR for pools', error);
+    return of({});
+  }),
+);
+
+export const [useFixedApr] = bind(poolAprs$, {});
