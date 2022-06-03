c04b18e9ab04baa2898d1b9dd3f584da2d61b11a
Lazar <Lazar__93@hotmail.com>
2022-06-03 12:14:21 +0200

    fix

--- packages/tempus-client_v3/src/hooks/useWalletAddress.ts
+++ packages/tempus-client_v3/src/hooks/useWalletAddress.ts
@@ -1,15 +1,16 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of, combineLatestWith, filter, tap, take } from 'rxjs';
 import { bind } from '@react-rxjs/core';
 
 export const walletAddress$ = new BehaviorSubject<string>('');
 
 export function setWalletAddress(address: string): void {
-  const currentAddress = walletAddress$.getValue();
-
-  // We don't want to set same address multiple times because that would
-  // trigger unnecessary fetch in other hooks that depend on wallet address
-  if (currentAddress !== address) {
-    walletAddress$.next(address);
-  }
+  of(address)
+    .pipe(
+      combineLatestWith(walletAddress$),
+      filter(([newAddress, currentAddress]) => newAddress !== currentAddress),
+      tap(([newAddress]) => walletAddress$.next(newAddress)),
+      take(1),
+    )
+    .subscribe();
 }
 export const [useWalletAddress] = bind(walletAddress$);
