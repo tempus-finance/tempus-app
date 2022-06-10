65a2745631432a9cbf6e5a629acf5342ab07cef6
Lazar Mitic <Lazar__93@hotmail.com>
2022-06-10 18:01:23 +0200

    update

--- packages/tempus-core-services/webpack.config.js
+++ packages/tempus-core-services/webpack.config.js
@@ -1,31 +1,22 @@
-// Webpack uses this to work with directories
 const path = require('path');
 const webpack = require('webpack');
 
-require('dotenv').config({ path: '../tempus-client_v3/.env.development' });
+require('dotenv').config({ path: './.env.production' });
 
-// This is the main configuration object.
-// Here, you write different options and tell Webpack what to do
 module.exports = {
-  // Path to your entry point. From this file Webpack will begin its work
   entry: './src/index.ts',
-
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: [/node_modules/],
-        include: [path.resolve(__dirname, 'src')],
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
@@ -33,17 +24,8 @@ module.exports = {
     library: 'tempus-core-services',
     libraryTarget: 'umd',
   },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on the final bundle. For now, we don't need production's JavaScript
-  // minifying and other things, so let's set mode to development
-  mode: 'development',
-
+  mode: 'production',
   devtool: 'eval-source-map',
-
-  watch: true,
-
   plugins: [
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env),
