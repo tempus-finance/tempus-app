652e756735b82c08a5249c40c65aeca19c36db3f
Vukašin Manojlović <iamvukasin@gmail.com>
2022-06-10 12:06:06 +0200

    Add optional back button to pools heading

--- packages/tempus-client_v3/src/components/shared/PoolsHeading/PoolsHeading.spec.tsx
+++ packages/tempus-client_v3/src/components/shared/PoolsHeading/PoolsHeading.spec.tsx
@@ -1,5 +1,7 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import PoolsHeading, { PoolsHeadingProps } from './PoolsHeading';
+
+const mockOnBackButtonClick = jest.fn();
 
 const defaultProps: PoolsHeadingProps = {
   text: 'Ethereum-network pools',
@@ -15,5 +17,29 @@ describe('PoolsHeading', () => {
 
     expect(actualText).not.toBeNull();
     expect(container).toMatchSnapshot();
+  });
+
+  it('renders a heading label with provided text and back button', () => {
+    const { container, getByText, getByRole } = subject({ ...defaultProps, onBackButtonClick: mockOnBackButtonClick });
+
+    const actualText = getByText('Ethereum-network pools');
+    const actualBackButton = getByRole('button');
+
+    expect(actualText).not.toBeNull();
+    expect(actualBackButton).not.toBeNull();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('calls `onBackButtonClick` when the back button is clicked', () => {
+    const { getByRole } = subject({ ...defaultProps, onBackButtonClick: mockOnBackButtonClick });
+
+    const actualBackButton = getByRole('button');
+
+    expect(actualBackButton).not.toBeNull();
+
+    fireEvent.click(actualBackButton);
+
+    expect(mockOnBackButtonClick).toHaveBeenCalledTimes(1);
+    expect(actualBackButton).toMatchSnapshot();
   });
 });
