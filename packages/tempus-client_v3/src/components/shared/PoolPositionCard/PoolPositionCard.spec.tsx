01e3d22a0bcf4f848318d312e5235abd31ab5e5e
Lazar <Lazar__93@hotmail.com>
2022-06-20 15:59:34 +0200

    Fix tests

--- packages/tempus-client_v3/src/components/shared/PoolPositionCard/PoolPositionCard.spec.tsx
+++ packages/tempus-client_v3/src/components/shared/PoolPositionCard/PoolPositionCard.spec.tsx
@@ -4,8 +4,8 @@ import I18nProvider from '../../../i18n/I18nProvider';
 import PoolPositionCard, { PoolPositionCardProps } from './PoolPositionCard';
 
 const defaultProps: PoolPositionCardProps = {
-  apr: 0.1,
-  term: new Date(Date.UTC(2022, 2, 4)),
+  apr: new Decimal(0.1),
+  term: new Date(Date.UTC(2024, 2, 4)),
   profitLoss: new Decimal(10.29651),
   balance: new Decimal(20.9294),
   totalYieldEarned: new Decimal(1.12345),
