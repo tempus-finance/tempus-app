98493956b38f900d45eb83d97ec24035e025c5d1
irinelT <irinel@tempus.finance>
2022-06-21 12:38:25 +0200

    fixes 3

--- integration-testing/utility/constants.ts
+++ integration-testing/utility/constants.ts
@@ -3,7 +3,9 @@ export const USER_DATA_DIR: string = 'integration-testing/utility/user_data';
 export const METAMASK_PATH: string = 'integration-testing/mm';
 export const METAMASK_ID_PATH: string = 'integration-testing/utility/mm_id.txt';
 export const TEMPUS_URL: string
-  = config.CI || config.LOCALHOST ? 'http://localhost:3000' : 'https://tempus-app-stage.web.app/';
+  = config.CI && config.LOCALHOST ? 'http://172.20.0.3:3000' :
+    config.LOCALHOST ? 'http://localhost:3000' : 'https://tempus-app-stage.web.app/';
+
 //export const TEMPUS_URL: string = 'https://tempus-app-stage.web.app/';
 export const LOAD_TIMEOUT: number = 600;
 export const LOAD_SHORT_TIMEOUT: number = 200;
