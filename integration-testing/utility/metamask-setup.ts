347da0cd33ddf40fedf5e131537f0c89085491dd
irinelT <irinel@tempus.finance>
2022-06-21 16:44:25 +0200

    typo in mm-setup

--- integration-testing/utility/metamask-setup.ts
+++ integration-testing/utility/metamask-setup.ts
@@ -1,4 +1,4 @@
-import { BrowserContext, Page } from '@playwright/test';
+import { BrowserContext } from '@playwright/test';
 import { metamaskRegister, metamaskLogin } from '../modules/metamask';
 import { chromiumPersistant } from '../modules/browser';
 import { tempusMetamaskConnect } from '../modules/tempushome';
@@ -6,16 +6,16 @@ import { ETH_NETWORK, tempusNetworkChange } from '../modules/network';
 
 (async () => {
   const browser: BrowserContext = await chromiumPersistant();
-  const page: Page = await browser.pages()[0];
+  //const page: Page = await browser.pages()[0];
   await metamaskRegister(browser);
   await metamaskLogin(browser);
-  await page.pause();
+  //await page.pause();
   await tempusMetamaskConnect(browser);
   // TODO: add all metamask accounts and switch to 0, currently not needed
   //await metamaskAddETHfork(browser);
   //await metamaskAccountsAddAll(browser);
   //await metamaskAccountSwitch(browser, 1);
   await tempusNetworkChange(browser, ETH_NETWORK.name);
-  await page.pause();
+  //await page.pause();
   await browser.close();
 })();
