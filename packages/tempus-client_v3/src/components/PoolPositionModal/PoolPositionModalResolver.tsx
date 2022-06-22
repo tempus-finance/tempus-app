d20a415287f6ca4cd1d00e8185cfa065ea9d8a04
Lazar Mitic <Lazar__93@hotmail.com>
2022-06-22 09:31:34 +0200

    Use actual maturity data inside pool position modal (#1834)

--- packages/tempus-client_v3/src/components/PoolPositionModal/PoolPositionModalResolver.tsx
+++ packages/tempus-client_v3/src/components/PoolPositionModal/PoolPositionModalResolver.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { Chain, Decimal, ProtocolName, Ticker, ZERO } from 'tempus-core-services';
+import { getConfigManager } from '../../config/getConfigManager';
 import { useFixedAprs, usePoolBalance } from '../../hooks';
 import { PoolPositionModal } from './PoolPositionModal';
 
@@ -12,7 +13,15 @@ export const PoolPositionModalResolver: FC = () => {
 
   // TODO - Properly check if URL params have valid values - if not show an error page or redirect to root page
 
-  if (!poolAddress) {
+  const poolData = useMemo(() => {
+    if (!poolAddress) {
+      return null;
+    }
+
+    return getConfigManager().getPoolData(poolAddress);
+  }, [poolAddress]);
+
+  if (!poolData) {
     // TODO - Show 404 page (pool not found)
     return null;
   }
@@ -20,7 +29,7 @@ export const PoolPositionModalResolver: FC = () => {
   return (
     // TODO - Replace dummy data with data from hooks
     <PoolPositionModal
-      apr={fixedAprs[poolAddress]}
+      apr={fixedAprs[`${poolData.chain}-${poolData.address}`]}
       balance={poolBalanceData?.balanceInBackingToken || ZERO}
       chartData={[
         { x: new Date(2022, 3, 4), y: 10 },
@@ -28,7 +37,7 @@ export const PoolPositionModalResolver: FC = () => {
       ]}
       profitLoss={new Decimal(2)}
       projectedTotalYield={new Decimal(3)}
-      term={new Date(2022, 4, 2)}
+      term={new Date(poolData.maturityDate)}
       tokenDecimals={4}
       tokenExchangeRate={new Decimal(100)}
       totalYieldEarned={new Decimal(2)}
