5733bcabbdb897fdb823e1c955e6c50a0cb81141
Vukašin Manojlović <iamvukasin@gmail.com>
2022-06-23 13:15:32 +0200

    Add markets footer (#1880)
    * Add markets footer
    * Update translations

--- packages/tempus-client_v3/src/components/Markets/Markets.tsx
+++ packages/tempus-client_v3/src/components/Markets/Markets.tsx
@@ -2,12 +2,16 @@ import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import MarketsSubheader from './MarketsSubheader';
 import MarketsPools from './MarketsPools';
+import MarketsSwitchNetworkFooter from './MarketsSwitchNetworkFooter';
 import './Markets.scss';
 
 const Markets: FC = () => (
   <div className="tc__app__markets">
-    <MarketsSubheader />
-    <MarketsPools />
+    <div className="tc__app__markets-content">
+      <MarketsSubheader />
+      <MarketsPools />
+    </div>
+    <MarketsSwitchNetworkFooter />
     <Outlet />
   </div>
 );
