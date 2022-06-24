bc192bf50116d8378cac3f6f42fe584d0a06d07d
ShawTim <shawtim.liu@gmail.com>
2022-06-24 15:22:20 +0800

    remove unsed dateFormatter in constant (#1893)

--- packages/tempus-client_v3/src/constants.ts
+++ packages/tempus-client_v3/src/constants.ts
@@ -1,7 +1,3 @@
 export const POLLING_INTERVAL_IN_MS = 2 * 60 * 1000;
 export const DEBOUNCE_IN_MS = 500;
 export const TIMEOUT_FROM_SUCCESS_TO_DEFAULT_IN_MS = 3000;
-
-export const dateFormatter = new Intl.DateTimeFormat('en-GB', {
-  dateStyle: 'short',
-});
