8cc5b899d54fa185e2194f067566b9c8527c6a91
Lazar Mitic <Lazar__93@hotmail.com>
2022-06-27 16:26:51 +0200

    Add website_v2 scaffold

---
+++ packages/website_v2/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div>Tempus Website V2.0</div>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
