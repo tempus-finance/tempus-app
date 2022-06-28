092fbc32fff2b4707195eaf4fdaf1b87cfb42cd3
Daniele Damiani <danieledamianiuk@gmail.com>
2022-06-28 17:13:07 +0100

    Add `Blog` section

--- packages/website_v2/src/components/App/App.tsx
+++ packages/website_v2/src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import Header from '../Header';
 import Hero from '../Hero';
+import Blog from '../Blog';
 import Footer from '../Footer';
 import Investors from '../Investors';
 
@@ -8,6 +9,7 @@ const App = (): JSX.Element => (
     <Header />
     <Hero />
     <Investors />
+    <Blog />
     <Footer />
   </>
 );
