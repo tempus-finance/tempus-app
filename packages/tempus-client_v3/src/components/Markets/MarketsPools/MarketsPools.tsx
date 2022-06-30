854e5d1d70349097e1c971c82d40124d89ce6555
Vukašin Manojlović <iamvukasin@gmail.com>
2022-06-30 13:25:48 +0200

    Show empty placeholder when there are no cards in markets

--- packages/tempus-client_v3/src/components/Markets/MarketsPools/MarketsPools.tsx
+++ packages/tempus-client_v3/src/components/Markets/MarketsPools/MarketsPools.tsx
@@ -1,8 +1,9 @@
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Chain, prettifyChainName, ProtocolName, Ticker } from 'tempus-core-services';
 import { useChainList, useFilteredSortedPoolList, useSelectedChain } from '../../../hooks';
-import { PoolCardData, PoolCardGrid, PoolCardStatus, PoolsHeading } from '../../shared';
+import { PoolCardData, PoolCardGrid, PoolCardStatus, PoolsHeading, Typography } from '../../shared';
 
 import './MarketsPools.scss';
 
@@ -11,6 +12,7 @@ const MarketsPools = (): JSX.Element => {
   const chains = useChainList();
   const tempusPools = useFilteredSortedPoolList();
   const [selectedChain] = useSelectedChain();
+  const { t } = useTranslation();
 
   /**
    * If user wallet is connected and selected chain is available we want to show pools only from selected network,
@@ -38,62 +40,85 @@ const MarketsPools = (): JSX.Element => {
     }
   };
 
-  return (
-    <div className="tc__marketsPools-container">
-      {availableChains.map(chain => {
-        const prettyChainName = prettifyChainName(chain);
+  const chainCardsMap = useMemo(() => {
+    const cardsMap = new Map<Chain, PoolCardData[]>();
 
-        // Create a list of cards for Markets page, one card per backing token
-        // TODO - Create separate cards for Fixed and Boosted yields
-        const chainCards: PoolCardData[] = [];
-        tempusPools.forEach(pool => {
-          // Skip pools from other chains
-          if (pool.chain !== chain) {
-            return;
-          }
-
-          // We want to show one card per matured pool on markets page
-          if (pool.maturityDate < Date.now()) {
-            chainCards.push({
-              pools: [pool],
-              chain: pool.chain,
-              token: pool.backingToken,
-              tokenAddress: pool.backingTokenAddress,
-              protocol: pool.protocol,
-              matured: true,
-            });
-            return;
-          }
-
-          const tokenCard = chainCards.find(
-            card => card.token === pool.backingToken && card.protocol === pool.protocol && !card.matured,
-          );
-          if (tokenCard) {
-            tokenCard.pools.push(pool);
-          } else {
-            chainCards.push({
-              pools: [pool],
-              chain: pool.chain,
-              token: pool.backingToken,
-              tokenAddress: pool.backingTokenAddress,
-              protocol: pool.protocol,
-              matured: false,
-            });
-          }
-        });
-
-        // In case chain does not have any cards - skip showing it
-        if (chainCards.length === 0) {
-          return null;
+    availableChains.forEach(chain => {
+      // Create a list of cards for Markets page, one card per backing token
+      // TODO - Create separate cards for Fixed and Boosted yields
+      const chainCards: PoolCardData[] = [];
+      tempusPools.forEach(pool => {
+        // Skip pools from other chains
+        if (pool.chain !== chain) {
+          return;
         }
 
-        return (
-          <div key={chain}>
-            <PoolsHeading text={`${prettyChainName}-Network pools`} />
-            <PoolCardGrid chain={chain} cards={chainCards} cardVariant="markets" onCardClick={handleClick} />
-          </div>
+        // We want to show one card per matured pool on markets page
+        if (pool.maturityDate < Date.now()) {
+          chainCards.push({
+            pools: [pool],
+            chain: pool.chain,
+            token: pool.backingToken,
+            tokenAddress: pool.backingTokenAddress,
+            protocol: pool.protocol,
+            matured: true,
+          });
+          return;
+        }
+
+        const tokenCard = chainCards.find(
+          card => card.token === pool.backingToken && card.protocol === pool.protocol && !card.matured,
         );
-      })}
+        if (tokenCard) {
+          tokenCard.pools.push(pool);
+        } else {
+          chainCards.push({
+            pools: [pool],
+            chain: pool.chain,
+            token: pool.backingToken,
+            tokenAddress: pool.backingTokenAddress,
+            protocol: pool.protocol,
+            matured: false,
+          });
+        }
+      });
+
+      cardsMap.set(chain, chainCards);
+    });
+
+    return cardsMap;
+  }, [availableChains, tempusPools]);
+
+  const cardsAvailable = useMemo(() => [...chainCardsMap.values()].flat().length > 0, [chainCardsMap]);
+
+  return (
+    <div className="tc__marketsPools-container">
+      {cardsAvailable &&
+        [...chainCardsMap.entries()].map(([chain, chainCards]) => {
+          const prettyChainName = prettifyChainName(chain);
+
+          // In case chain does not have any cards - skip showing it
+          if (chainCards.length === 0) {
+            return null;
+          }
+
+          return (
+            <div key={chain}>
+              <PoolsHeading text={`${prettyChainName}-Network pools`} />
+              <PoolCardGrid chain={chain} cards={chainCards} cardVariant="markets" onCardClick={handleClick} />
+            </div>
+          );
+        })}
+      {!cardsAvailable && (
+        <div className="tc__marketsPools-empty">
+          <div className="tc__marketsPools-empty-content">
+            <Typography variant="subtitle" weight="bold">
+              {t('MarketsPools.emptyTitle')}
+            </Typography>
+            <Typography variant="subtitle">{t('MarketsPools.emptyDescription')}</Typography>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
