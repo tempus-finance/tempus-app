0a9cc9c5cbbafa24f33d37ecdfdbe00ac034efe0
Lazar Mitic <Lazar__93@hotmail.com>
2022-07-05 12:21:16 +0200

    Fix hash link navigation (#2025)

--- packages/website_v2/src/components/shared/Link/Link.tsx
+++ packages/website_v2/src/components/shared/Link/Link.tsx
@@ -15,22 +15,34 @@ const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
 
   const isExternal = useMemo(() => href.includes('://') && new URL(href).origin !== window.location.origin, [href]);
 
-  return isExternal ? (
-    <a
-      ref={ref}
-      rel="external noreferrer nofollow"
-      target="_blank"
-      className={`tw__link ${className}`}
-      title={title}
-      href={href}
-      onClick={onClick}
-    >
-      {children}
-    </a>
-  ) : (
-    <InternalLink ref={ref} className={`tw__link ${className}`} title={title} to={href} onClick={onClick}>
-      {children}
-    </InternalLink>
+  const isHash = useMemo(() => href.startsWith('#'), [href]);
+
+  return (
+    <>
+      {isExternal && (
+        <a
+          ref={ref}
+          rel="external noreferrer nofollow"
+          target="_blank"
+          className={`tw__link ${className}`}
+          title={title}
+          href={href}
+          onClick={onClick}
+        >
+          {children}
+        </a>
+      )}
+      {isHash && (
+        <a ref={ref} className={`tw__link ${className}`} title={title} href={href} onClick={onClick}>
+          {children}
+        </a>
+      )}
+      {!isExternal && !isHash && (
+        <InternalLink ref={ref} className={`tw__link ${className}`} title={title} to={href} onClick={onClick}>
+          {children}
+        </InternalLink>
+      )}
+    </>
   );
 });
 
