384c2efd1c6ca704b15481f276132681e53d44a4
Vukašin Manojlović <iamvukasin@gmail.com>
2022-07-05 12:44:01 +0200

    Make header responsive (#2026)
    * Make header responsive
    * Add more TODOs

--- packages/website_v2/src/components/Header/Header.tsx
+++ packages/website_v2/src/components/Header/Header.tsx
@@ -1,28 +1,60 @@
+import { useCallback, useState } from 'react';
 import { Link } from '../shared';
+import { MenuIcon } from './icons';
 import './Header.scss';
 
-const Header = (): JSX.Element => (
-  <div className="tw__header">
-    <div>
-      <Link href="/">
-        <img src="/images/header-logo.svg" alt="Tempus DAO" />
-      </Link>
+const Header = (): JSX.Element => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const handleMenuOpen = useCallback(() => setMenuOpened(true), []);
+
+  const handleMenuClose = useCallback(() => setMenuOpened(false), []);
+
+  const handleLinkClick = useCallback(() => {
+    setTimeout(() => {
+      handleMenuClose();
+    }, 200);
+  }, [handleMenuClose]);
+
+  return (
+    <div className="tw__header">
+      <div className="tw__header-logo">
+        <Link href="/">
+          <img src="/images/header-logo.svg" alt="Tempus DAO" />
+        </Link>
+      </div>
+      {/* TODO: Add hover style for menu button */}
+      <button type="button" className="tw__btn tw__header-menu-button" onClick={handleMenuOpen}>
+        <MenuIcon />
+      </button>
+      <div className={`tw__header-links ${menuOpened ? 'tw__header-menu-opened' : ''}`}>
+        {/* TODO: Replace with close icon when the design is ready */}
+        {/* TODO: Add hover style for close button */}
+        {menuOpened && (
+          <button type="button" className="tw__btn tw__header-close-button" onClick={handleMenuClose}>
+            Close
+          </button>
+        )}
+        {/* TODO - Add click handlers */}
+        <Link className="tw__header-link tw__hover-animation" onClick={handleLinkClick}>
+          Products
+        </Link>
+        <Link className="tw__header-link tw__hover-animation" href="/team" onClick={handleLinkClick}>
+          Contributors
+        </Link>
+        <Link className="tw__header-link tw__hover-animation" onClick={handleLinkClick}>
+          Token
+        </Link>
+        <Link className="tw__header-link tw__hover-animation" onClick={handleLinkClick}>
+          Docs
+        </Link>
+        <Link className="tw__header-link tw__hover-animation" onClick={handleLinkClick}>
+          Blog
+        </Link>
+      </div>
+      <div className="tw__header-separator" />
     </div>
-    <div className="tw__header-links">
-      {/* TODO - Add click handlers */}
-      <p className="tw__header-link tw__hover-animation">Products</p>
-      <Link className="tw__header-link tw__hover-animation" href="/team">
-        Contributors
-      </Link>
-      <p className="tw__header-link tw__hover-animation">Token</p>
-      <Link className="tw__header-link tw__hover-animation" href="https://docs.tempus.finance/">
-        Docs
-      </Link>
-      <Link className="tw__header-link tw__hover-animation" href="https://medium.com/tempusfinance">
-        Blog
-      </Link>
-    </div>
-    <div className="tw__header-separator" />
-  </div>
-);
+  );
+};
+
 export default Header;
