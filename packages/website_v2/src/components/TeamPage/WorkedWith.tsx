9f2a34b5d3adc822131e67b1b41388f1204fa79b
ShawTim <shawtim.liu@gmail.com>
2022-07-06 16:25:39 +0800

    color fixes on team page

--- packages/website_v2/src/components/TeamPage/WorkedWith.tsx
+++ packages/website_v2/src/components/TeamPage/WorkedWith.tsx
@@ -5,7 +5,7 @@ const WorkedWith = (): JSX.Element => (
     <h2 className="tw__team__worked-with__title">
       Our team has worked with some of the world&apos;s leading organizations
     </h2>
-    <div className="tw__team__separator" />
+    <div className="tw__team__worked-with__separator" />
     <div className="tw__team__worked-with__organizations">
       <CompanyLogo variant="linklaters" />
       <CompanyLogo variant="ethereum-foundation" />
