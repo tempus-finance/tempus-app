2035394fb6c56c4db3f1ffc254ccee748c954916
Lazar <Lazar__93@hotmail.com>
2022-07-06 14:12:07 +0200

    Fix team page elements style

--- packages/website_v2/src/components/TeamPage/WorkedWith.tsx
+++ packages/website_v2/src/components/TeamPage/WorkedWith.tsx
@@ -3,7 +3,7 @@ import CompanyLogo from './logos/CompanyLogo';
 const WorkedWith = (): JSX.Element => (
   <div className="tw__team__work-with">
     <h2 className="tw__team__worked-with__title">
-      Our team has worked with some of the world&apos;s leading organizations
+      Our team has experience from working with some of the world&apos;s leading organizations
     </h2>
     <div className="tw__team__worked-with__separator" />
     <div className="tw__team__worked-with__organizations">
