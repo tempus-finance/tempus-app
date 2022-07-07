10d55357e08666673fd546a7f77c99739ad473b1
Lazar <Lazar__93@hotmail.com>
2022-07-07 11:43:58 +0200

    Make token/hero section responsive

--- packages/website_v2/src/components/TokenomicsPage/TokenomicsHeader.tsx
+++ packages/website_v2/src/components/TokenomicsPage/TokenomicsHeader.tsx
@@ -3,6 +3,9 @@ import Header from '../Header';
 
 const TokenomicsHeader = (): JSX.Element => (
   <div className="tw__tokenomics__header">
+    <div className="tw__tokenomics__header__background-container">
+      <div className="tw__tokenomics__header__background" />
+    </div>
     <Header color="transparent" />
     <div className="tw__tokenomics__header__title">
       <span className="tw__tokenomics__header__title-text">Own a slice of</span>
