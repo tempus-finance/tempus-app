7ce7a27b3f82b2ab049c763a866b2403a977c923
Vukašin Manojlović <iamvukasin@gmail.com>
2022-07-07 16:01:01 +0200

    Add governance steps (#2096)

---
+++ packages/website_v2/src/components/GovernancePage/illustrations/index.ts
@@ -0,0 +1,5 @@
+import DiscussIllustration from './DiscussIllustration';
+import ProposeIllustration from './ProposeIllustration';
+import VoteIllustration from './VoteIllustration';
+
+export { DiscussIllustration, ProposeIllustration, VoteIllustration };
