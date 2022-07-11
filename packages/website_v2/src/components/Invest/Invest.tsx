9461b7fe77be8f5f9a60d10a175a891df1a505a9
Daniele Damiani <danieledamianiuk@gmail.com>
2022-07-11 12:34:35 +0100

    Update `Invest in the future` section

--- packages/website_v2/src/components/Invest/Invest.tsx
+++ packages/website_v2/src/components/Invest/Invest.tsx
@@ -1,8 +1,8 @@
 import { memo } from 'react';
 
 import CoinGeckoIcon from '../../icons/CoinGeckoIcon';
-import CoinMarketCapIcon from '../../icons/CoinMarketCapIcon';
-import { LinkBlock, ScrollFadeIn } from '../shared';
+import { ArrowRight, CoinMarketCapIcon } from '../../icons';
+import { Link, LinkBlock, ScrollFadeIn } from '../shared';
 import InvestBackground from './InvestBackground';
 
 import './Invest.scss';
@@ -14,12 +14,20 @@ const Invest = (): JSX.Element => (
     </div>
     <div className="tw__container tw__invest__container">
       <ScrollFadeIn>
-        <h2 className="tw__invest__title">Invest in the future</h2>
-        <div className="tw__invest__content">
-          <p className="tw__invest__description">
-            Tempus DAO is governed by TEMP token holders. TEMP token holders capture value from all products built by
-            Tempus.
-          </p>
+        <h2 className="tw__invest__title">Powered by TEMP</h2>
+        <div className="tw__invest__body">
+          <div className="tw__invest__description">
+            <p>
+              Tempus DAO is governed by TEMP token holders. TEMP token holders capture value from all products built by
+              Tempus.
+            </p>
+            <div className="tw__invest__read-more">
+              <Link href="/token" className="tw__hover-animation">
+                Read more about TEMP
+                <ArrowRight />
+              </Link>
+            </div>
+          </div>
           <div className="tw__invest__links">
             <LinkBlock href="https://www.coingecko.com/en/coins/tempus" title="CoinGecko" icon={<CoinGeckoIcon />} />
             <LinkBlock
