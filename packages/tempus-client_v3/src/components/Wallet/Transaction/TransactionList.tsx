efaec3d6e05e6cb362073cb54f013e90fdcfb96e
ShawTim <shawtim.liu@gmail.com>
2022-07-22 19:58:22 +0800

    show more/less for the transaction history on wallet popup (#2241)

--- packages/tempus-client_v3/src/components/Wallet/Transaction/TransactionList.tsx
+++ packages/tempus-client_v3/src/components/Wallet/Transaction/TransactionList.tsx
@@ -1,22 +1,33 @@
-import { FC, memo } from 'react';
+import { FC, memo, useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { usePoolMap } from '../../../hooks';
 import { Notification } from '../../../interfaces/Notification';
-import { Typography } from '../../shared';
+import { ButtonWrapper, Icon, Typography } from '../../shared';
 import TransactionItem from './TransactionItem';
 
 interface TransactionListProps {
   transactions: Notification[];
 }
 
+const DEFAULT_DISPLAY_ITEMS = 3;
+const MAX_DISPLAY_ITEMS = 10;
+
 const TransactionList: FC<TransactionListProps> = props => {
   const { transactions } = props;
   const { t } = useTranslation();
   const poolMap = usePoolMap();
 
+  const [showMore, setShowMore] = useState<boolean>(false);
+  const handleShowMore = useCallback(() => setShowMore(true), []);
+  const handleShowLess = useCallback(() => setShowMore(false), []);
+  const displayItems = useMemo(
+    () => transactions.slice(0, showMore ? MAX_DISPLAY_ITEMS : DEFAULT_DISPLAY_ITEMS),
+    [showMore, transactions],
+  );
+
   return (
     <div className="tc__wallet-popup__transaction-list">
-      {transactions.map(transaction => (
+      {displayItems.map(transaction => (
         <TransactionItem
           key={transaction.id}
           transaction={transaction}
@@ -25,6 +36,26 @@ const TransactionList: FC<TransactionListProps> = props => {
       ))}
       {!transactions.length && (
         <Typography variant="body-secondary">{t('TransactionList.descriptionNoTransactions')}</Typography>
+      )}
+      {transactions.length >= DEFAULT_DISPLAY_ITEMS && !showMore && (
+        <div className="tc__wallet-popup__transaction-list__show-more">
+          <ButtonWrapper onClick={handleShowMore}>
+            <Typography variant="body-secondary" weight="bold">
+              {t('TransactionList.buttonShowMore')}
+            </Typography>
+            <Icon variant="down-chevron" size="tiny" />
+          </ButtonWrapper>
+        </div>
+      )}
+      {transactions.length >= DEFAULT_DISPLAY_ITEMS && showMore && (
+        <div className="tc__wallet-popup__transaction-list__show-more">
+          <ButtonWrapper onClick={handleShowLess}>
+            <Typography variant="body-secondary" weight="bold">
+              {t('TransactionList.buttonShowLess')}
+            </Typography>
+            <Icon variant="up-chevron" size="tiny" />
+          </ButtonWrapper>
+        </div>
       )}
     </div>
   );
